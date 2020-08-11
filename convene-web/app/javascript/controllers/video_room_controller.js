c234d2095c83abacea386f40ebf84a56219008bd
Tom Lee <user512@users.noreply.github.com>
2020-08-11 11:03:57 -0700

    Move end call button back to jitsi iframe

--- convene-web/app/javascript/controllers/video_room_controller.js
+++ convene-web/app/javascript/controllers/video_room_controller.js
@@ -9,6 +9,9 @@ export default class extends Controller {
     this.videoRoom.addEventListener('enteredRoom', () => {
       this.wrapperTarget.classList.add('active-room');
     });
+    this.videoRoom.addEventListener('exitedRoom', () => {
+      window.location.replace(this.data.get('workspacePath'));
+    });
 
     this.videoRoom.enterRoom(this.data.get('name'));
   }
