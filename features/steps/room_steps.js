6d758dbce1a66472a515e14bcbab67cbe6199ab7
Tom Lee <user512@users.noreply.github.com>
2020-08-14 12:42:43 -0700

    Test member can enter room when tab enter room.

--- features/steps/room_steps.js
+++ features/steps/room_steps.js
@@ -1,4 +1,6 @@
 const { Given, When, Then } = require("cucumber");
+const { By, Key, until } = require('selenium-webdriver');
+const assert = require('assert').strict;
 
 Given("a Workspace with a {accessLevel} {room}", function (accessLevel, room) {
   // Write code here that turns the phrase above into concrete actions
@@ -14,6 +16,20 @@ When("a {actor} provides the correct {room} Key", function (actor, room) {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
 });
+
+When('I tap the {room} in the {room} Picker', async function (room, room2) {
+  // TODO: How to find a card with a specific room name then click enter room?
+  const roomCard = this.driver.findElement(By.partialLinkText('Enter Room'))
+  await roomCard.click()
+});
+
+Then('I am in the {room}', async function (room) {
+  this.driver.wait(until.elementLocated(By.css("[name='jitsiConferenceFrame0']")));
+
+  const jitsiIframe = await this.driver.findElements(By.css("[name='jitsiConferenceFrame0']"))
+  assert.equal(jitsiIframe.length, 1, 'Jitsi iframe not found');
+});
+
 
 Then("the {actor} is placed in the {room}", function (actor, room) {
   // Write code here that turns the phrase above into concrete actions
