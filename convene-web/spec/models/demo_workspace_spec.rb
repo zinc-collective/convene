408bdc525daadda3bfd1243f6ed93024731f0969
Tom Lee <user512@users.noreply.github.com>
2020-08-22 21:24:42 -0700

    Test on Github actions
    Run RSpec and e2e test on Github actions
    Run rails server on port 80 to test branded domain
    If we run e2e test when server is running on port 80 and
    edit /etc/host with 127.0.0.1 system-test.zinc.local,
    then the driver can simply navigate to http://system-test.zinc.local,
    and user can click enter room and reach http://system-test.zinc.local/room-slug.

--- convene-web/spec/models/demo_workspace_spec.rb
+++ convene-web/spec/models/demo_workspace_spec.rb
@@ -2,26 +2,39 @@ require "rails_helper"
 
 RSpec.describe DemoWorkspace, type: :model do
   describe ".prepare" do
-    it "creates a workspace suitable for demoing Convene" do
-      demo_workspace = DemoWorkspace.prepare
-      aggregate_failures do
-        expect(demo_workspace).to be_persisted
-        expect(demo_workspace).to be_unlocked
-        expect(demo_workspace.access_code).to be_nil
-        expect(demo_workspace.name).to eql("Convene Demo")
-        expect(demo_workspace.slug).to eql("convene-demo")
-        expect(demo_workspace.jitsi_meet_domain).to eql("convene-videobridge-zinc.zinc.coop")
-        expect(demo_workspace.branded_domain).to eql("convene-demo.zinc.coop")
+    context "When the environment is set to enable demo" do
+      before { allow(ENV).to receive(:[]).with('DEMO_ENABLED').and_return('true') }
 
-        expect(demo_workspace.rooms.find_by(name: "Zee's Desk")).to be_present
-        expect(demo_workspace.rooms.find_by(name: "Zee's Desk")).to be_listed
-        expect(demo_workspace.rooms.find_by(name: "Vivek's Desk")).to be_present
-        expect(demo_workspace.rooms.find_by(name: "Vivek's Desk")).to be_listed
-        expect(demo_workspace.rooms.find_by(name: "Water Cooler")).to be_present
-        expect(demo_workspace.rooms.find_by(name: "Water Cooler")).to be_listed
+      it "creates a workspace suitable for demoing Convene" do
+        demo_workspace = DemoWorkspace.prepare
+        aggregate_failures do
+          expect(demo_workspace).to be_persisted
+          expect(demo_workspace).to be_unlocked
+          expect(demo_workspace.access_code).to be_nil
+          expect(demo_workspace.name).to eql("Convene Demo")
+          expect(demo_workspace.slug).to eql("convene-demo")
+          expect(demo_workspace.jitsi_meet_domain).to eql("convene-videobridge-zinc.zinc.coop")
+          expect(demo_workspace.branded_domain).to eql("convene-demo.zinc.coop")
 
-        expect(demo_workspace.rooms.find_by(name: "The Ada Lovelace Room")).to be_present
-        expect(demo_workspace.rooms.find_by(name: "The Ada Lovelace Room")).to be_listed
+          expect(demo_workspace.rooms.find_by(name: "Zee's Desk")).to be_present
+          expect(demo_workspace.rooms.find_by(name: "Zee's Desk")).to be_listed
+          expect(demo_workspace.rooms.find_by(name: "Vivek's Desk")).to be_present
+          expect(demo_workspace.rooms.find_by(name: "Vivek's Desk")).to be_listed
+          expect(demo_workspace.rooms.find_by(name: "Water Cooler")).to be_present
+          expect(demo_workspace.rooms.find_by(name: "Water Cooler")).to be_listed
+
+          expect(demo_workspace.rooms.find_by(name: "The Ada Lovelace Room")).to be_present
+          expect(demo_workspace.rooms.find_by(name: "The Ada Lovelace Room")).to be_listed
+        end
+      end
+    end
+
+    context "When the environment is set to disable demo" do
+      before { allow(ENV).to receive(:[]).with('DEMO_ENABLED').and_return(nil) }
+      it "does not create demo workspace" do
+        expect do
+          DemoWorkspace.prepare
+        end.not_to change {[ Workspace.count, Client.count, Room.count] }
       end
     end
 
