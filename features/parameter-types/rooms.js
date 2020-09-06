e02f7e3d2560c2f55126b614a5b9f7d42f40f05d
Zee <50284+zspencer@users.noreply.github.com>
2020-09-06 11:53:25 -0700

    🔧 Steps may match unnamed Rooms with the Room parameter type
    This through us for a bit of a loop, because we didn't understand how
    parameterTypes got their arguments for their Transformers. We did some
    digging into the underlying Cucumber-JS code-base; to figure out what
    may be happening; and discovered that paramterType matching discards the
    full content of the match in cases where there are child matches.

--- features/parameter-types/rooms.js
+++ features/parameter-types/rooms.js
@@ -4,8 +4,20 @@ const { defineParameterType } = require("cucumber");
 // steps that mention `Room` in isolation.
 defineParameterType({
   name: "room",
-  regexp: /"([^"]*)" ?Room/,
-  transformer: (room) => new Room(room),
+  // To make the Room Name optional, we had to capture two things, otherwise
+  // Cucumber will discard the "room" value. This appears to be because the
+  // `Group` class assumes that if there are capture-groups in a parameterType,
+  // the rest of the match should be discarded.
+  // See: https://github.com/cucumber/cucumber/blob/5a3a3167958c45d708228d953a1d5b0f5625a633/cucumber-expressions/javascript/src/Group.ts#L11
+  //
+  // There are probably multiple ways to work around this. For example, instead of
+  // adding a capture group, we could check if roomName is undefined in the Room
+  // class, and default it to 'Room' or something.
+  //
+  // In the meantime, adding a capture-group around "Room" ensures that the Room
+  // class has a string provided to it.
+  regexp: /("[^"]*" )?(Room)/,
+  transformer: (roomName) => new Room(roomName),
 });
 
 class Room {
