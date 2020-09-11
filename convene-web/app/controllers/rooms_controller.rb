c5ea3e470f842c5632935b887d6b1993eaa9bfc5
Tom Lee <user512@users.noreply.github.com>
2020-09-10 17:57:13 -0700

    Extract current_room into helper method to avoid leaking instance variable into views

--- convene-web/app/controllers/rooms_controller.rb
+++ convene-web/app/controllers/rooms_controller.rb
@@ -1,15 +1,9 @@
 class RoomsController < ApplicationController
-  before_action :set_current_room
-
   def show
-    if @current_room.enterable?(params[:access_code])
+    if current_room.enterable?(params[:access_code])
       render layout: "video_room"
     else
       render 'waiting_room'
     end
-  end
-
-  private def set_current_room
-    @current_room ||= current_workspace.rooms.friendly.find(params[:id] || params[:room_id])
   end
 end
