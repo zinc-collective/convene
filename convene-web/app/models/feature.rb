ec53f0f5fef32384e2a9fbb425b3cef8f599050b
Tom Lee <user512@users.noreply.github.com>
2020-10-18 15:40:24 -0700

    Hide configure room feature behind a feature flag

--- convene-web/app/models/feature.rb
+++ convene-web/app/models/feature.rb
@@ -7,6 +7,7 @@ class Feature
     feature = feature.to_sym if feature.respond_to?(:to_sym)
     return true if feature == :demo && ENV['DEMO_ENABLED']
     return true if feature == :system_test && ENV['SYSTEM_TEST']
+    return true if feature == :configure_room && ENV['CONFIGURE_ROOM']
 
     false
   end
