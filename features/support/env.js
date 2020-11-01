dec832c30bcff1d6ca31388cb2264ed5180bc713
Tom Lee <user512@users.noreply.github.com>
2020-11-01 14:11:21 -0800

    Attach screenshot when test case failed

--- features/support/env.js
+++ features/support/env.js
@@ -1,9 +1,10 @@
-const { setWorldConstructor, After, setDefaultTimeout } = require('cucumber');
+const { setWorldConstructor, After, setDefaultTimeout, Status } = require('cucumber');
 const { Builder } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 
 class CustomWorld {
-  constructor() {
+  constructor({ attach }) {
+    this.attach = attach;
     this.driver = new Builder()
       .forBrowser('firefox')
       .setFirefoxOptions(this.firefoxOption())
@@ -19,7 +20,14 @@ class CustomWorld {
 
 setWorldConstructor(CustomWorld);
 
-After(function() {
+After(function(testCase) {
+  if (testCase.result.status == Status.FAILED) {
+    return this.driver.takeScreenshot().then( screenShot => {
+      console.log('screenshot', screenShot)
+      this.attach(screenShot, 'image/png');
+      this.driver.quit()
+    });
+  }
   this.driver.quit();
 });
 
