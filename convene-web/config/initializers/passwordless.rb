84bac5dc8cf1b45a5c12122a3b659b69921af2f5
Tom Lee <user512@users.noreply.github.com>
2020-11-21 16:15:07 -0800

    wip - styling stuff

---
+++ convene-web/config/initializers/passwordless.rb
@@ -0,0 +1,5 @@
+# Override default Passwordless gem configuration here.
+# https://github.com/mikker/passwordless#configuration
+
+Passwordless.restrict_token_reuse = true
+Passwordless.default_from_address = "support@convene.zinc.coop"
