18596bb6b6b171b878d18bdc3e8ed53571363e4f
Tom Lee <user512@users.noreply.github.com>
2021-01-02 22:11:43 -0800

    Avoid eager loading in test
    I tried a few things to avoid eager loading:
    1. Switch to classic loader instead of the default Rails 6 Zeitwerk
      * It works but classic loader will be deprecated eventually, not recommended.
    2. Rename Admin::ApplicationController to BaseController
      * I wasn't 100% why it works but my guess is Zeitwerk only made one autoload look up and
        the first one it found was Admin::ApplicationController, and raise the superclass mismatch error.
      * There might be a way to config Zeitwerk to do a smarter look up but I think renaming
        Admin::ApplicationController to Admin::BaseController makes sense, and we can treat
        ApplicationController as a Rails keyword instead.

--- convene-web/app/controllers/admin/rooms_controller.rb
+++ convene-web/app/controllers/admin/rooms_controller.rb
@@ -1,5 +1,5 @@
 module Admin
-  class RoomsController < Admin::ApplicationController
+  class RoomsController < Admin::BaseController
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
     #
