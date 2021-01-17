5aaab95cef78c738c71f38440ff4578681e3ca23
Tom Lee <user512@users.noreply.github.com>
2021-01-16 17:45:08 -0800

    Use primary/neutral colors + gradient per wireframes (#173)
    * Update background gradient, button color and header color
    * Leverage Tailwind color config to avoid hard coding color
    * Name button class using theme instead
    * Consolidate CSS classes to rely more on markup element
    
    Co-Authored-By: Tom Lee <user512@users.noreply.github.com>
    Co-Authored-By: Ana Ulin <anaulin@users.noreply.github.com>
    Co-Authored-By: Millie <churusaa@users.noreply.github.com>
    Co-authored-by: Whitney <roseaboveit@users.noreply.github.com>
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>

--- convene-web/tailwind.config.js
+++ convene-web/tailwind.config.js
@@ -1,4 +1,16 @@
+// TODO: Uncomment tailwindcss/colors upon upgrading to Tailwind 2
+// const colors = require('tailwindcss/colors')
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.purple,
+        neutral: colors.gray,
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/ui'),
   ]
