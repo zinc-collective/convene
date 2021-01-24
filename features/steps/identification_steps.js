339a03447d8c02b9b38b334a9fcb3ea4fdfec7fb
Whitney-Rose Levis <ms.whitney.levis@gmail.com>
2021-01-23 16:47:54 -0800

    Make SignInPage a local variable.

--- features/steps/identification_steps.js
+++ features/steps/identification_steps.js
@@ -2,24 +2,20 @@ const { Given, When, Then } = require("cucumber");
 const SignInPage = require("../page-objects/SignInPage");
 
 Given('a {actor} has Identified themselves using an Email Address', async function (actor) {
-    this.signInPage = new SignInPage(this.driver);
-    await this.signInPage.enter();
-    await this.signInPage.submitEmail("test@example.com");
+    const signInPage = new SignInPage(this.driver);
+    await signInPage.enter();
+    await signInPage.submitEmail("test@example.com");
  });
-
 
  When('the {actor} opens the Identification Verification Link emailed to them', function (actor) {
    // Write code here that turns the phrase above into concrete actions
    return 'pending';
  });
 
-
  Then('the {actor} is Verified as the Owner of that Email Address', function (actor) {
    // Write code here that turns the phrase above into concrete actions
    return 'pending';
  });
-
-
 
  Then('the {actor} has become Authenticated', function (actor) {
    // Write code here that turns the phrase above into concrete actions
