9c05c77950e94dd0d3958397a7fd869830241e78
CJ Joulain <cj@zinc.coop>
2021-03-07 11:31:04 -0800

    InvitationController#create persists Invitation for current_space
    Added redirect after invitation creation
    Updated spec for redirect action
    
    Co-authored-by: Kelly Hong <KellyAH@users.noreply.github.com>
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Egbet Abraha <eg-bet@users.noreply.github.com>
    Co-authored-by: Tom Lee <user512@users.noreply.github.com>

--- convene-web/app/controllers/invitations_controller.rb
+++ convene-web/app/controllers/invitations_controller.rb
@@ -2,6 +2,12 @@ class InvitationsController < ApplicationController
 
   # TODO: fix create action to create invitation data so spec test passes
   def create
+    @space_invitation = current_space.invitations.new(invitation_params)
+    @space_invitation.save!
+    redirect_to [current_space, @space_invitation]
+  end
 
+  def invitation_params
+    params.require(:invitation).permit(:name, :email)
   end
 end
--- convene-web/spec/requests/spaces/invitations_request_spec.rb
+++ convene-web/spec/requests/spaces/invitations_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "/spaces/:space_id/invitations", type: :request do
-  it "doesn't expsose" do
+  it "doesn't expose" do
     client = Client.create!
     space = Space.create!(:client => client, :name => "bar")
 
     post "/spaces/#{space.slug}/invitations", :params => { :invitation => {:name => "foobar", :email => "foobar@example.com"} }
-    expect(response).to be_ok
+    expect(response).to redirect_to([space, space.invitations.find_by(name: 'foobar', email: 'foobar@example.com')])
     expect(space.invitations.find_by(name: 'foobar', email: 'foobar@example.com')).to be_present
   end
 end
