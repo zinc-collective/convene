64045064656fa7e46e70ef81221232e359325a67
Tom Lee <user512@users.noreply.github.com>
2021-03-21 22:10:44 -0700

    Revert "Favor one liner RSpec"
    This reverts commit ce0becf269b24368ea42d191dc440be997c4c0ec.

--- convene-web/spec/tasks/release_after_build_spec.rb
+++ convene-web/spec/tasks/release_after_build_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "rake release:after_build" do
-  subject do
+  subject(:run_rake_task!) do
     ConveneWeb::Application.load_tasks
     Rake::Task["release:after_build"].reenable
     Rake::Task["release:after_build"].execute
   end
 
-  it { is_expected.not_to raise_error }
+  it "does not fail" do
+    expect { run_rake_task! }.not_to raise_error
+  end
 end
