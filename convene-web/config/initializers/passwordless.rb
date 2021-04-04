bd2652fef510f11a34a3cc459c806e02a5f9a852
Ana Ulin <ana@ulin.org>
2021-04-04 10:16:19 -0700

    Send Passwordless links to the domain that initiated the Sign In (#223)
    See: https://github.com/zinc-collective/convene/issues/118

--- convene-web/config/initializers/passwordless.rb
+++ convene-web/config/initializers/passwordless.rb
@@ -6,3 +6,9 @@ Passwordless.success_redirect_path = ENV["APP_ROOT_URL"]
 Passwordless.sign_out_redirect_path = ENV["APP_ROOT_URL"]
 # Expiry time for magic link
 Passwordless.timeout_at = lambda { 1.hour.from_now }
+
+Passwordless.after_session_save = lambda do |session, request|
+  # Replace default Passwordless Mailer with our own, where we make sure
+  # to direct the user to the correct (sub)domain.
+  MagicLinkMailer.magic_link(session, request).deliver_now
+end
