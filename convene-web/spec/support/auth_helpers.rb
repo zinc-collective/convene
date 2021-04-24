74a10df8b35ed88b8fe327b98a451825efe24543
Ana Ulin <ana@ulin.org>
2021-04-24 15:58:36 -0700

    Add AuthHelpers, so that we can sign in from a spec.

---
+++ convene-web/spec/support/auth_helpers.rb
@@ -0,0 +1,12 @@
+module AuthHelpers
+  # Borrowed from https://github.com/mikker/passwordless/blob/master/lib/passwordless/test_helpers.rb, which
+  # is not released yet.
+  def sign_in(person)
+    session = Passwordless::Session.create!(authenticatable: person, user_agent: "TestAgent", remote_addr: "unknown")
+    get Passwordless::Engine.routes.url_helpers.token_sign_in_path(session.token)
+  end
+end
+
+RSpec.configure do |config|
+  config.include AuthHelpers
+end
