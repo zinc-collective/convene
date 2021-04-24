07eea48aa9db1020a47007fe1f3e22f282a311be
Ana Ulin <ana@ulin.org>
2021-04-24 15:59:13 -0700

    Authorize updates to Furniture Placements in controller.

--- convene-web/spec/factories/furniture_placement.rb
+++ convene-web/spec/factories/furniture_placement.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :furniture_placement do
     room
     furniture_kind { "markdown_text_block" }
+    settings { { content: "# Original Content"} }
     slot { 1 }
   end
 end
