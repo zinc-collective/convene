d7f4432e0b501b397ce7ee862ab9878930dfc0ae
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2021-04-28 05:24:09 +0000

    Add appropriate attr_accessors

--- convene-web/app/lib/utilities/utility.rb
+++ convene-web/app/lib/utilities/utility.rb
@@ -21,6 +21,7 @@ module Utilities
     # Standard permissions for how Utilities can be managed.
     # Can be overridden on a per-Utility basis.
     class Policy
+      attr_accessor :person, :utility
       def initialize(person, utility)
         self.person = person
         self.utility = utility
