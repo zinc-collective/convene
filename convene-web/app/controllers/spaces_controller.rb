4096308241ed04bfadc5b94f92c267a5606c7210
Zee <50284+zspencer@users.noreply.github.com>
2021-05-07 20:54:01 -0700

    Make the root present a space for Convene by default (#251)
    Co-authored-by: Ana Ulin <ana@ulin.org>

--- convene-web/app/controllers/spaces_controller.rb
+++ convene-web/app/controllers/spaces_controller.rb
@@ -2,11 +2,6 @@ class SpacesController < ApplicationController
   def show; end
 
   helper_method def space
-    @space ||= BrandedDomain.new(space_repository).space_for_request(request) ||
-               space_repository.friendly.find(params[:id])
-  end
-
-  def space_repository
-    Space.includes(:rooms, entrance: [:furniture_placements])
+    @space ||= current_space
   end
 end
