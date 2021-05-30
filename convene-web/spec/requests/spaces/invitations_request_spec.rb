4112f5c3fc8f58c7ad0772c80a1ed5b969c82b01
Kelly Hong <k_ahong@yahoo.com>
2021-05-30 12:08:33 -0700

    clean up comments

--- convene-web/spec/requests/spaces/invitations_request_spec.rb
+++ convene-web/spec/requests/spaces/invitations_request_spec.rb
@@ -21,16 +21,15 @@ RSpec.describe "/spaces/:space_id/invitations", type: :request do
     expect(invitation.status).to eq("sent")
   end
 
-
-  # We decided that possible invitation status:
-  # pending - email was not sent yet
+  # ------ #
+  # NOTES
+  # ------ #
+  # Kelly H, Zee, Egbet decided these are the possible invitation statuses:
+  # pending - email was not sent yet due to mailer issues etc.
   # sent - email was sent but not accepted
-  # accepted - reciever accepted it
-  # rejected - reciever rejected the invitation
-  # FUTURE: expired -
-  #
-  # ALL CREATE INVITATION SCENARIOS  - POST CALL
-  # invitation email is sent
-  #
-  # send invitation to same email if old one expired
+  # accepted - receiver accepted the invitation
+  # rejected - receivers rejected the invitation
+
+  # FUTURE STATUS AND TEST:
+  # expired status and tests for testing the possible scenarios
 end
