0a728f21636d6c480c85d84a33fd71fb150e181f
KellyAH <KellyAH@users.noreply.github.com>
2021-05-30 12:18:40 -0700

    Continue fleshing out the test for creating a space invitation (#283)
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Egbet Abraha <eg-bet@users.noreply.github.com>

--- convene-web/spec/requests/spaces/invitations_request_spec.rb
+++ convene-web/spec/requests/spaces/invitations_request_spec.rb
@@ -1,12 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "/spaces/:space_id/invitations", type: :request do
-  it "doesn't expose" do
+  it "created and sends an invitation for a space" do
     client = Client.create!
     space = Space.create!(:client => client, :name => "bar")
+    mail = double(deliver_now: true)
+    allow(SpaceInvitationMailer).to receive(:space_invitation_email).and_return(mail)
 
     post "/spaces/#{space.slug}/invitations", :params => { :invitation => {:name => "foobar", :email => "foobar@example.com"} }
-    expect(response).to redirect_to([space, space.invitations.find_by(name: 'foobar', email: 'foobar@example.com')])
-    expect(space.invitations.find_by(name: 'foobar', email: 'foobar@example.com')).to be_present
+
+    invitation = space.invitations.find_by(name: 'foobar', email: 'foobar@example.com')
+
+    expect(response).to redirect_to([space, invitation])
+    expect(invitation).to be_present
+
+    # TODO finish building logic to get below assertions to pass
+    # generate mailer and views
+    expect(SpaceInvitationMailer).to have_received(:space_invitation_email).with(invitation)
+    expect(mail).to have_received(:deliver_now)
+    expect(invitation.status).to eq("sent")
   end
+
+  # ------ #
+  # NOTES
+  # ------ #
+  # Kelly H, Zee, Egbet decided these are the possible invitation statuses:
+  # pending - email was not sent yet due to mailer issues etc.
+  # sent - email was sent but not accepted
+  # accepted - receiver accepted the invitation
+  # rejected - receivers rejected the invitation
+
+  # FUTURE STATUS AND TEST:
+  # expired status and tests for testing the possible scenarios
 end
