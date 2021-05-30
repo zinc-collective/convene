17191d3a737fb5d12a67f488b7a47ee3e199631d
Zee <50284+zspencer@users.noreply.github.com>
2021-05-30 12:32:30 -0700

    Add a MailerPreview for the AuthenticatedSessionMailer
    Turns out we had some weird-ass leftover tests that did not make sense,
    so I purged them and made a new mailer preview for the mailer we _do_
    have

--- convene-web/spec/mailers/previews/test_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/test_mailer
-class TestMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/test_mailer/welcome_email
-  def welcome_email
-    TestMailer.welcome_email
-  end
-
-end
