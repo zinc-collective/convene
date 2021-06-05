32ed38923e8207a78d34b16bd4a1b509092d9c4f
Zee <50284+zspencer@users.noreply.github.com>
2021-06-05 16:13:08 -0700

    Allow email's to be delivered from a real address (#284)
    * Allow email's to be delivered from a real address
    
    This way folks can reply and ask for help and such
    
    - Fixes `convene-web/bin/setup` to copy `convene-web/.env.example`
    when `convene-web/.env` does not exist.
    - Fixes the default URL builder to use `protocol` and not `scheme` when
      building URLs
    
    * Add a MailerPreview for the AuthenticatedSessionMailer
    
    Turns out we had some weird-ass leftover tests that did not make sense,
    so I purged them and made a new mailer preview for the mailer we _do_
    have

--- convene-web/config/initializers/action_mailer.rb
+++ convene-web/config/initializers/action_mailer.rb
@@ -17,5 +17,5 @@ if ENV["APP_ROOT_URL"]
   root_uri = URI.parse(ENV["APP_ROOT_URL"])
   ActionMailer::Base.default_url_options[:host] = root_uri.host
   ActionMailer::Base.default_url_options[:port] = root_uri.port if root_uri.port != root_uri.default_port
-  ActionMailer::Base.default_url_options[:scheme] = root_uri.scheme
+  ActionMailer::Base.default_url_options[:protocol] = root_uri.scheme
 end
