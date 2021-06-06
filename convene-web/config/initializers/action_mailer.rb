6ab2557a5ed55f6393f3f42a307af4ae417d0ef1
Ana Ulin <ana@ulin.org>
2021-06-05 18:20:07 -0700

    Surround initializer call to ActionMailer::Base in Rails.application.reloader.to_prepare to remove deprecation warnings related to autoloading.

--- convene-web/config/initializers/action_mailer.rb
+++ convene-web/config/initializers/action_mailer.rb
@@ -1,21 +1,23 @@
-if ENV["SMTP_ADDRESS"]
-  ActionMailer::Base.smtp_settings = {
-    address: ENV["SMTP_ADDRESS"],
-    domain: ENV["SMTP_DOMAIN"],
-    port: ENV["SMTP_PORT"].to_i,
-    user_name: ENV["SMTP_USERNAME"],
-    password: ENV["SMTP_PASSWORD"],
-    authentication: ENV["SMTP_AUTHENTICATION"]&.to_sym,
-    tls: ENV.fetch("SMTP_ENABLE_TLS", true) != "false",
-    enable_starttls_auto: ENV.fetch("SMTP_ENABLE_TLS", true) != "false"
-  }.compact
+Rails.application.reloader.to_prepare do
+  if ENV["SMTP_ADDRESS"]
+    ActionMailer::Base.smtp_settings = {
+      address: ENV["SMTP_ADDRESS"],
+      domain: ENV["SMTP_DOMAIN"],
+      port: ENV["SMTP_PORT"].to_i,
+      user_name: ENV["SMTP_USERNAME"],
+      password: ENV["SMTP_PASSWORD"],
+      authentication: ENV["SMTP_AUTHENTICATION"]&.to_sym,
+      tls: ENV.fetch("SMTP_ENABLE_TLS", true) != "false",
+      enable_starttls_auto: ENV.fetch("SMTP_ENABLE_TLS", true) != "false"
+    }.compact
 
-  ActionMailer::Base.delivery_method = :smtp
-end
+    ActionMailer::Base.delivery_method = :smtp
+  end
 
-if ENV["APP_ROOT_URL"]
-  root_uri = URI.parse(ENV["APP_ROOT_URL"])
-  ActionMailer::Base.default_url_options[:host] = root_uri.host
-  ActionMailer::Base.default_url_options[:port] = root_uri.port if root_uri.port != root_uri.default_port
-  ActionMailer::Base.default_url_options[:protocol] = root_uri.scheme
+  if ENV["APP_ROOT_URL"]
+    root_uri = URI.parse(ENV["APP_ROOT_URL"])
+    ActionMailer::Base.default_url_options[:host] = root_uri.host
+    ActionMailer::Base.default_url_options[:port] = root_uri.port if root_uri.port != root_uri.default_port
+    ActionMailer::Base.default_url_options[:protocol] = root_uri.scheme
+  end
 end
