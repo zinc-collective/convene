bc6546a59a1e86a8e25cd332c14dbd6f590434b5
Zee <50284+zspencer@users.noreply.github.com>
2021-07-11 11:00:34 -0700

    Clean up old feature flags and Admin config (#314)
    This was used when we had Administrate, but we don't use Administrate
    anymore.
    
    We also want to prefix all feature flag environment variables with
    FEATURE_
    
    Co-authored-by: Kelly Hong <KellyAH@users.noreply.github.com>

--- convene-web/app/models/feature.rb
+++ convene-web/app/models/feature.rb
@@ -8,12 +8,6 @@ class Feature
   def self.enabled?(feature)
     feature = feature.to_s if feature.respond_to?(:to_s)
 
-    # @todo remove these after 6/1/2021
-    return true if feature == 'demo' && ENV['DEMO_ENABLED']
-    return true if feature == 'system_test' && ENV['SYSTEM_TEST']
-    return true if feature == 'configure_room' && ENV['CONFIGURE_ROOM']
-    return true if feature == 'identification' && ENV['IDENTIFICATION']
-
     ENV.fetch("FEATURE_#{feature.upcase}", 'false') == 'true'
   end
 end
--- convene-web/spec/models/feature_spec.rb
+++ convene-web/spec/models/feature_spec.rb
@@ -8,19 +8,5 @@ RSpec.describe Feature do
       expect(Feature.enabled?(:bar)).not_to be_truthy
       expect(Feature.enabled?(:baz)).not_to be_truthy
     end
-
-    it 'supports the legacy feature flag environment names (for now)' do
-      stub_const('ENV',
-                 { 'DEMO_ENABLED' => 'true',
-                   'SYSTEM_TEST' => 'true',
-                   'CONFIGURE_ROOM' => 'true',
-
-                   'IDENTIFICATION' => 'true' })
-
-      expect(Feature.enabled?(:demo)).to be_truthy
-      expect(Feature.enabled?(:system_test)).to be_truthy
-      expect(Feature.enabled?(:identification)).to be_truthy
-      expect(Feature.enabled?(:configure_room)).to be_truthy
-    end
   end
 end
