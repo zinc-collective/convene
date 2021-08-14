ff014f97412f281cf51ef27843f922183d9cd72e
Zee <50284+zspencer@users.noreply.github.com>
2021-08-14 15:07:14 -0700

    Check Drop Box: Exchange Plaid's Public Token for Access Token (#326)
    See: 
    
    * Refactor: Move Plaid API Client onto Plaid Utility
    
    So other pieces of furniture that use Plaid don't have to
    instantiate the client.
    
    * Show the selected Account in a Form with the Amount
    * Wire in the rest of the Plaid deposit flow
    
    - Adds a Checks controller to instantiate a Check
    - Exchanges the Plaid Item Public Token for an Access Token
    
    Co-authored-by: Ana Ulin <ana@ulin.org>

--- convene-web/app/lib/utilities/plaid.rb
+++ convene-web/app/lib/utilities/plaid.rb
@@ -4,6 +4,23 @@ module Utilities
   # Provides Plaid functionality to a {Space}
   # @see https://plaid.com
   class Plaid < Utility
+    # @return [Plaid::PlaidApi]
+    def plaid_client
+      @plaid_client ||= ::Plaid::PlaidApi.new(
+        ::Plaid::ApiClient.new(plaid_configuration)
+      )
+    end
+
+    def plaid_configuration
+      ::Plaid::Configuration.new do |configuration|
+        configuration.server_index =
+          ::Plaid::Configuration::Environment[environment]
+        configuration.api_key['PLAID-CLIENT-ID'] = client_id
+        configuration.api_key['PLAID-SECRET'] = secret
+        configuration.api_key['Plaid-Version'] = version
+      end
+    end
+
     def client_id
       configuration['client_id']
     end
@@ -21,21 +38,29 @@ module Utilities
     end
 
     def environment
-      configuration['environment']
+      configuration['environment']&.downcase&.to_s
     end
 
     def environment=(environment)
-      configuration['environment']=environment
+      configuration['environment'] = environment
+    end
+
+    def version=(version)
+      configuration['version'] = version
+    end
+
+    def version
+      configuration.fetch('version', '2020-09-14')
     end
 
     def attribute_names
-      super + [:environment, :secret, :client_id]
+      super + [:environment, :secret, :client_id, :version]
     end
   end
 
   class PlaidPolicy < Utility::Policy
     def permitted_params
-      [:environment, :secret, :client_id]
+      [:environment, :secret, :client_id, :version]
     end
   end
 end
