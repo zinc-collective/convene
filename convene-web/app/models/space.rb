03dec5a43e0f35d3981031a5ac14606d37e3d496
Zee@Zinc <50284+zspencer@users.noreply.github.com>
2021-10-11 03:42:59 +0000

    Neighborhood Operators may set a Default Space
    Mostly I've been wishing I could go to system-test by default when I'm futzing about.

--- convene-web/app/models/space.rb
+++ convene-web/app/models/space.rb
@@ -52,7 +52,7 @@ class Space < ApplicationRecord
   # A room's Access Code is a "secret" that, when known, grants access to the room.
   attribute :access_code, :string
 
-  scope :default, -> { friendly.find('convene') }
+  scope :default, -> { friendly.find(ENV.fetch('DEFAULT_SPACE', 'convene')) }
 
   def unlocked?
     access_level&.to_sym != :locked
