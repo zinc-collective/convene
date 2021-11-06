274e3b3f0ec748d1ab10a16cf2081baa9b928eeb
Ana Ulin <ana@ulin.org>
2021-11-06 16:50:07 -0700

    Add and configure Sentry. (#435)

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,11 @@
+Sentry.init do |config|
+  next unless ENV['SENTRY_DSN']
+
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, hint|
+    filter.filter(event.to_hash)
+  end
+end
