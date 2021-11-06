8a373c17772de00f87550ea09a3bb214afd1022c
Ana Ulin <ana@ulin.org>
2021-11-06 13:21:13 -0700

    Add policy scope to Invitation.

--- app/policies/invitation_policy.rb
+++ app/policies/invitation_policy.rb
@@ -16,4 +16,18 @@ class InvitationPolicy < ApplicationPolicy
   alias update? create?
   alias destroy? create?
   alias new? create?
+
+  class Scope
+    attr_accessor :actor, :scope
+
+    def initialize(actor, scope)
+      self.actor = actor
+      self.scope = scope
+    end
+
+    # All invitations for an accessible space are visible.
+    def resolve
+      scope.all
+    end
+  end
 end
