2b558b9b8388c77fab613d16c76cb94734d0e2ff
Tom Lee <user512@users.noreply.github.com>
2021-12-19 15:47:08 -0800

    Dont' check precomile asset on test, is this a right move?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,6 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
+
+  config.assets.check_precompiled_asset = false
 end
