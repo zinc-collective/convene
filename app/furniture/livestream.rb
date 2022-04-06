d556e6e9d35eeddfb0857468d490474d14551a95
Zee Spencer <zspencer@users.noreply.github.com>
2022-04-05 20:15:26 -0700

    Twitch Livestream may be embedded in a Room
    See: https://github.com/zinc-collective/convene/issues/632
    
    This sprouts a new Livestream Furniture, and makes it available to
    Rooms.

---
+++ app/furniture/livestream.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Renders a Twitch Livestream in a Room
+class Livestream
+  include Placeable
+  def channel=(channel)
+    settings['channel'] = channel
+  end
+
+  def channel
+    settings.fetch('channel', '')
+  end
+
+  def provider=(provider)
+    settings['provider'] = provider
+  end
+
+  def provider
+    settings.fetch('provider', 'twitch')
+  end
+
+  def layout=(layout)
+    settings['layout'] = layout
+  end
+
+  def layout
+    settings.fetch('layout', '')
+  end
+
+  # @todo can we make it so we don't need to define this?
+  # and the `settings.fetch` bits?
+  def attribute_names
+    super + %w[channel layout]
+  end
+end
