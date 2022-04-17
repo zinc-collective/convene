a452681890d260a12d693bbe45ef13e7ebe112b4
Zee Spencer <zspencer@users.noreply.github.com>
2022-04-17 10:34:06 -0700

    Tidy up AuthenticationMethod parameter permissions
    Co-authored-by: Neer Malathapa <nirmalathapa@users.noreply.github.com>
    Co-authored-by: Ana Ulin <anaulin@users.noreply.github.com>
    Co-authored-by: Naomi Quinones <naomiquinones@users.noreply.github.com>

--- app/controllers/authentication_methods_controller.rb
+++ app/controllers/authentication_methods_controller.rb
@@ -4,6 +4,7 @@
 class AuthenticationMethodsController < ApplicationController
   def create
     skip_policy_scope
+    authentication_method_params = policy(AuthenticationMethod).permit(params)
     authentication_method = AuthenticationMethod.new(authentication_method_params)
     authorize(authentication_method)
     if authentication_method.save
@@ -11,9 +12,5 @@ class AuthenticationMethodsController < ApplicationController
     else
       render json: AuthenticationMethod::Serializer.new(authentication_method).to_json, status: :unprocessable_entity
     end
-  end
-
-  def authentication_method_params
-    params.require(:authentication_method).permit(:contact_method, :contact_location)
   end
 end
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Parent class for Policies.
+# @see https://github.com/varvet/pundit#policies
 class ApplicationPolicy
   attr_reader :person, :object
 
