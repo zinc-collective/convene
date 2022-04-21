f928a5f062dfd3ba78119a7375d66c936a698275
Ana Ulin <ana@ulin.org>
2022-04-20 20:01:48 -0700

    Use more inteligible `host` method instead of `domain(length)`
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>

--- app/models/branded_domain.rb
+++ app/models/branded_domain.rb
@@ -1,12 +1,13 @@
 class BrandedDomain
   attr_accessor :space_repository
+
   def initialize(space_repository)
     self.space_repository = space_repository
   end
 
   # Retrieves a Space
   def space_for_request(request)
-    branded_spaces[request.domain(1_000)]
+    branded_spaces[request.host]
   end
 
   # @return [TrueClass,FalseClass] True when a Space has a branded_domain that matches the request domain
@@ -16,9 +17,9 @@ class BrandedDomain
 
   # We cache the branded spaces to reduce the database-hits-per-request where we can.
   private def branded_spaces
-    Rails.cache.fetch("BrandedSubdomain#branded_spaces", expires_in: 1.minute) do
+    Rails.cache.fetch('BrandedSubdomain#branded_spaces', expires_in: 1.minute) do
       space_repository.where.not(branded_domain: nil).each_with_object({}) do |space, branded_domains|
-        branded_domains[space.branded_domain]=space
+        branded_domains[space.branded_domain] = space
       end
     end
   end
