94ddc706c1f33dea6572d9024ed9478df4ead5ba
Zee Spencer <zspencer@users.noreply.github.com>
2022-06-08 20:32:37 -0700

    Embedded Forms can accept Any Airtable Form URL!
    https://github.com/zinc-collective/convene/issues/619
    
    I got tired of having to go through the airtable menus to get the code
    from their iframe embed document, and I wanted a win!

---
+++ spec/furniture/embedded_form_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EmbeddedForm do
+  describe '#embeddable_form_url' do
+    it 'converts Airtable Share URLs into the Embed url' do
+      embedded_form = build(:embedded_form, form_url: 'https://airtable.com/shrCnrBzflvzDIlvg')
+      expect(embedded_form.embeddable_form_url).to eql('https://airtable.com/embed/shrCnrBzflvzDIlvg')
+
+      embedded_form =  build(:embedded_form, form_url: 'https://airtable.com/shrCnrBzflvzDIlvg/embed')
+      expect(embedded_form.embeddable_form_url).to eql('https://airtable.com/embed/shrCnrBzflvzDIlvg')
+    end
+  end
+end
