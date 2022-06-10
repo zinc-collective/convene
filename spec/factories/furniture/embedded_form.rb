e94f37f1029c7f98dbf92dd0c34703051a1e1fac
Zee <50284+zspencer@users.noreply.github.com>
2022-06-10 12:36:43 -0700

    Embedded Forms can accept Any Airtable Form URL! (#711)
    https://github.com/zinc-collective/convene/issues/619
    
    I got tired of having to go through the airtable menus to get the code
    from their iframe embed document, and I wanted a win!
    
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>

---
+++ spec/factories/furniture/embedded_form.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :embedded_form do
+    transient do
+      room { build(:room) }
+    end
+
+    placement do
+      association :furniture_placement, { furniture_kind: 'embedded_form', room: room }
+    end
+  end
+end
