f3d0a9b431d8f506b25fb42adc7f421a2afba481
Zee <50284+zspencer@users.noreply.github.com>
2022-07-25 17:51:48 -0700

    `dom_id` is useful when building turbo-streams (#765)
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>

--- app/furniture/furniture_controller.rb
+++ app/furniture/furniture_controller.rb
@@ -8,4 +8,12 @@ class FurnitureController < ::ApplicationController
   helper_method def space
     current_space
   end
+
+  def dom_id(record)
+    record_identifier.dom_id(record)
+  end
+
+  def record_identifier
+    @record_identifier ||= ActionView::RecordIdentifier
+  end
 end
