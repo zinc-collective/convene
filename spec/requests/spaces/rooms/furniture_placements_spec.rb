d8c563c4d0d098c74c92d4123e2198b3c7e060a2
Ana Ulin <ana@ulin.org>
2022-09-21 17:30:55 -0700

    Replace string interpolation with proper path construction.
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Sadie <19538219+sadiejay@users.noreply.github.com>

--- spec/requests/spaces/rooms/furniture_placements_spec.rb
+++ spec/requests/spaces/rooms/furniture_placements_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe '/spaces/:space_slug/room/:room_slug/furniture_placements', type:
     before { sign_in(space, person) }
     it "creates a furniture placement of the kind of furniture provided within the room" do
       expect do
-        post "/spaces/#{space.slug}/rooms/#{room.slug}/furniture_placements/", params: { furniture_placement: { furniture_kind: :markdown_text_block } }
+        post space_room_furniture_placements_path(space, room), params: { furniture_placement: { furniture_kind: :markdown_text_block } }
       end.to change { room.furniture_placements.count }.by(1)
 
       placement = room.furniture_placements.last
@@ -21,8 +21,9 @@ RSpec.describe '/spaces/:space_slug/room/:room_slug/furniture_placements', type:
       expect(response).to redirect_to([space, room])
     end
   end
+
   describe 'PATCH /spaces/:space_slug/rooms/:room_slug/furniture_placements/:id' do
-    let(:placement_path) { "/spaces/#{space.slug}/rooms/#{room.slug}/furniture_placements/#{placement.id}" }
+    let(:placement_path) { space_room_furniture_placement_path(space, room, placement) }
 
     context 'when the person is a guest' do
       it 'does not allow updating placements' do
@@ -46,7 +47,8 @@ RSpec.describe '/spaces/:space_slug/room/:room_slug/furniture_placements', type:
   end
 
   describe 'DELETE /spaces/:space_slug/rooms/:room_slug/furniture_placements/:id' do
-    let(:placement_path) { "/spaces/#{space.slug}/rooms/#{room.slug}/furniture_placements/#{placement.id}" }
+    let(:placement_path) { space_room_furniture_placement_path(space, room, placement) }
+
     context 'when the person is a guest' do
       it 'does not allow destroying placements' do
         patch placement_path, params: { furniture_placement: { furniture_attributes: { content: 'updated content' } } }
