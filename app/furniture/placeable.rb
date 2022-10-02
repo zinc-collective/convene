27d3661194e887923e19d084b0061f561d841d7d
Zee Spencer <zspencer@users.noreply.github.com>
2022-10-02 10:38:04 -0700

    Fix method lookup in class ancestry
    Co-authored-by: Neer Thapa <nirmalathapa@users.noreply.github.com>

--- app/furniture/placeable.rb
+++ app/furniture/placeable.rb
@@ -13,6 +13,28 @@ module Placeable
     placeable.include ActiveModel::Attributes
     placeable.include ActiveModel::AttributeAssignment
     placeable.extend ClassMethods
+    placeable.include InstanceMethods
+  end
+
+  # @todo is there a better way to not clobber our instance methods when we include stuff?
+  #       Perhaps what we want to do is switch `placeable` from a module to a class that
+  #       Furniture inherits from?
+  module InstanceMethods
+    def settings
+      placement.settings
+    end
+
+    # Allows us to `render furniture` and use the partial at `furniture/_furniture.html.erb`
+    # @example Assuming `spotlight` is a {Spotlight} that inherits {Placeable}
+    #      render spotlight # renders spotlight/_spotlight.html.erb
+    # @see https://api.rubyonrails.org/classes/ActiveModel/Conversion.html#method-i-to_partial_path
+    def to_partial_path
+      "#{self.class.furniture_kind}/#{self.class.furniture_kind}"
+    end
+
+    def form_template
+      "#{self.class.furniture_kind}/form"
+    end
   end
 
   module ClassMethods
@@ -23,21 +45,5 @@ module Placeable
     def furniture_kind
       @furniture_kind ||= name.demodulize.underscore
     end
-  end
-
-  def settings
-    placement.settings
-  end
-
-  # Allows us to `render furniture` and use the partial at `furniture/_furniture.html.erb`
-  # @example Assuming `spotlight` is a {Spotlight} that inherits {Placeable}
-  #      render spotlight # renders spotlight/_spotlight.html.erb
-  # @see https://api.rubyonrails.org/classes/ActiveModel/Conversion.html#method-i-to_partial_path
-  def to_partial_path
-    "#{self.class.furniture_kind}/#{self.class.furniture_kind}"
-  end
-
-  def form_template
-    "#{self.class.furniture_kind}/form"
   end
 end
