5c74d1963b8e283eb715e367c05e72c3a0a23bfa
Zee Spencer <50284+zspencer@users.noreply.github.com>
2022-10-23 16:48:53 -0700

    Zeitwerk eager-loading doesn't like gretel
    Apparently Gretel uses `instance_eval` to load it's breadcrumbs up;
    which is why `crumb` is available on `Object`.
    
    This is kinda a hacky-way to deal with that; but it may make sense for
    us to exclude the breadcrumb paths from autoloading somehow?

--- app/furniture/journal/breadcrumbs.rb
+++ app/furniture/journal/breadcrumbs.rb
@@ -1,17 +1,24 @@
 # frozen_string_literal: true
 
 # @see https://github.com/kzkn/gretel
-crumb :journal_entry do |entry|
-  parent :room, entry.room
-  link entry.headline, [entry.space, entry.room, entry]
-end
 
-crumb :new_journal_entry do |entry|
-  parent :room, entry.room
-  link 'Add a Journal Entry', [:new, entry.space, entry.room]
-end
+begin
+  crumb :journal_entry do |entry|
+    parent :room, entry.room
+    link entry.headline, [entry.space, entry.room, entry]
+  end
 
-crumb :edit_journal_entry do |entry|
-  parent :journal_entry, entry
-  link "Edit", [:edit, entry.space, entry.room, entry]
+  crumb :new_journal_entry do |entry|
+    parent :room, entry.room
+    link 'Add a Journal Entry', [:new, entry.space, entry.room]
+  end
+
+  crumb :edit_journal_entry do |entry|
+    parent :journal_entry, entry
+    link 'Edit', [:edit, entry.space, entry.room, entry]
+  end
+rescue NoMethodError => e
+  throw e unless e.message.start_with?("undefined method `crumb'")
+end
+module Journal::Breadcrumbs
 end
--- config/initializers/gretel.rb
+++ config/initializers/gretel.rb
@@ -1,2 +1,4 @@
-Gretel.breadcrumb_paths << Rails.root.join('app', 'furniture', '**', 'breadcrumbs.rb')
-Gretel.breadcrumb_paths << Rails.root.join('app', 'furniture', '**', 'breadcrumbs', '*.rb')
+Rails.application.config.to_prepare do
+  Gretel.breadcrumb_paths << Rails.root.join('app', 'furniture', '**', 'breadcrumbs.rb')
+  Gretel.breadcrumb_paths << Rails.root.join('app', 'furniture', '**', 'breadcrumbs', '*.rb')
+end
