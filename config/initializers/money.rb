830de39c86c5db344d97b69f36cb4ad9c3e16e68
Zee Spencer <50284+zspencer@users.noreply.github.com>
2022-11-09 18:50:21 -0800

    Marketplace: Cart can Total Price
    Co-Authored-By: Ana Ulin <anaulin@users.noreply.github.com>

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -70,7 +70,7 @@ MoneyRails.configure do |config|
   #
   # set to BigDecimal::ROUND_HALF_EVEN by default
   #
-  # config.rounding_mode = BigDecimal::ROUND_HALF_UP
+  config.rounding_mode = BigDecimal::ROUND_HALF_EVEN
 
   # Set default money format globally.
   # Default value is nil meaning "ignore this option".
--- spec/furniture/marketplace/cart_spec.rb
+++ spec/furniture/marketplace/cart_spec.rb
@@ -1,9 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Marketplace::Cart, type: :model do
-  it { is_expected.to have_many(:cart_products) }
+  it { is_expected.to have_many(:cart_products).dependent(:destroy) }
 
   it { is_expected.to have_many(:products).through(:cart_products) }
 
   it { is_expected.to belong_to(:marketplace).class_name("Marketplace::Marketplace") }
+
+  describe "#price_total" do
+    subject(:price_total) { cart.price_total }
+
+    let(:cart) { create(:marketplace_cart) }
+    let(:product_a) { create(:marketplace_product, marketplace: cart.marketplace) }
+    let(:product_b) { create(:marketplace_product, marketplace: cart.marketplace) }
+
+    before do
+      cart.cart_products.create!(product: product_a, quantity: 1)
+      cart.cart_products.create!(product: product_b, quantity: 2)
+    end
+
+    it { is_expected.to eql(product_a.price + product_b.price + product_b.price) }
+  end
 end
