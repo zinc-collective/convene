f95e5448296844a7951757ab244690dfb46377ed
Neer <nirmala@surf.net.np>
2022-11-13 21:03:40 +0100

    Marketplace: Add money field with tailwind stylings (#934)
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>

---
+++ spec/furniture/marketplace/product_policy_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe Marketplace::ProductPolicy do
+  describe '#permitted_attributes' do
+    subject(':permitted_attributes') { Marketplace::ProductPolicy.new(nil, nil).permitted_attributes }
+    it {is_expected.to include :price}
+  end
+end
