5391cb83f3e503c36408a97bd103e8afb5df07d0
Kelly Hong <k_ahong@yahoo.com>
2022-11-18 20:52:24 -0800

    WIP - add edit Marketplace::Product price spec test

--- spec/furniture/marketplace/product_spec.rb
+++ spec/furniture/marketplace/product_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Marketplace::Product, type: :model do
       product.price = 20
       expect(product.price_cents).to eql(2000)
     end
+
+    it('edits the price_cents') do
+      product = Marketplace::Product.new
+      product.price = 10
+      # TODO: FIX THIS TEST: edit the price via request response flow
+      product.price = 30
+      expect(product.price_cents).to eql(3000)
+    end
   end
 
 end
