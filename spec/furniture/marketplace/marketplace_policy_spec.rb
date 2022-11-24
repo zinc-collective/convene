5ed755bd56b68a0d9b04a55d066f0c2fb3ccc73a
Kelly Hong <k_ahong@yahoo.com>
2022-11-23 18:16:33 -0800

    enforced premissions for showing a Marketplace and Products
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Ana <anaulin@users.noreply.github.com>

---
+++ spec/furniture/marketplace/marketplace_policy_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Marketplace::MarketplacePolicy, type: :policy do
+  subject { described_class }
+
+  let(:marketplace) { create(:marketplace) }
+  let(:membership) { create(:membership, space: marketplace.room.space ) }
+  let(:member) { membership.member }
+  let(:non_member) { create(:person) }
+  let(:guest) { nil }
+
+  permissions :show? do
+    it { is_expected.to permit(member, marketplace) }
+    it { is_expected.to permit(non_member, marketplace) }
+    it { is_expected.to permit(guest, marketplace) }
+  end
+end
