7df7e40843e028cebd978dfa18d947822613b8a8
Kelly Hong <k_ahong@yahoo.com>
2022-11-23 17:23:37 -0800

    Marketplace::Marketplace and it's dependent carts can be destroyed
    Co-authored-by: Ana <anaulin@users.noreply.github.com>
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>

--- spec/furniture/marketplace/marketplace_spec.rb
+++ spec/furniture/marketplace/marketplace_spec.rb
@@ -2,5 +2,5 @@ require "rails_helper"
 
 RSpec.describe Marketplace::Marketplace, type: :model do
   it { is_expected.to have_many(:products).inverse_of(:marketplace) }
-  it { is_expected.to have_many(:carts).inverse_of(:marketplace) }
+  it { is_expected.to have_many(:carts).inverse_of(:marketplace).dependent(:destroy) }
 end
