d7824423b4b0353be6ad314ffba8f0c346e46e0e
Kelly Hong <k_ahong@yahoo.com>
2022-12-14 18:52:10 -0800

    Marketplace: expose Checkouts new page
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Ana <anaulin@users.noreply.github.com>

---
+++ app/furniture/marketplace/checkouts_controller.rb
@@ -0,0 +1,23 @@
+class Marketplace
+  class CheckoutsController < FurnitureController
+    def new
+      authorize(cart.build_checkout(shopper: shopper))
+    end
+
+    helper_method def shopper
+      @shopper ||= if current_person.is_a?(Guest)
+        Shopper.find_or_create_by(id: session[:current_cart] ||= SecureRandom.uuid)
+      else
+        Shopper.find_or_create_by(person: current_person)
+      end
+    end
+
+    helper_method def cart
+      @cart ||= marketplace.carts.find_or_create_by(shopper: shopper)
+    end
+
+    helper_method def marketplace
+      @marketplace ||= policy_scope(Marketplace).find(params[:marketplace_id])
+    end
+  end
+end
