25868ed425a55d756d783cd34902d0c223c8b8a2
Zee <50284+zspencer@users.noreply.github.com>
2022-12-21 09:38:57 -0800

    Journal: Entries with @handle@example.com link to https://example.com/@handle (#998)
    * Journal: Entries with @handle@example.com link to https://example.com/@handle
    
    https://github.com/zinc-collective/convene/issues/898
    
    There's probably a better way to do this that takes into account that
    handles may not use the same format on every fediverse instance; but
    we'll cross that bridge when we come to it.

--- app/furniture/journal/entry.rb
+++ app/furniture/journal/entry.rb
@@ -1,49 +1,62 @@
-class Journal::Entry < ApplicationRecord
-  self.table_name = "journal_entries"
-  include RendersMarkdown
-  extend StripsNamespaceFromModelName
+class Journal
+  class Entry < ApplicationRecord
+    self.table_name = "journal_entries"
+    include RendersMarkdown
+    extend StripsNamespaceFromModelName
 
-  scope :recent, -> { order("published_at DESC NULLS FIRST") }
+    scope :recent, -> { order("published_at DESC NULLS FIRST") }
 
-  attribute :headline, :string
-  attribute :body, :string
-  validates :body, presence: true
+    attribute :headline, :string
+    attribute :body, :string
+    validates :body, presence: true
 
-  # URI-friendly description of the entry.
-  attribute :slug, :string
-  validates :slug, uniqueness: {scope: :journal_id}
+    # URI-friendly description of the entry.
+    attribute :slug, :string
+    validates :slug, uniqueness: {scope: :journal_id}
 
-  # FriendlyId does the legwork to make the slug uri-friendly
-  extend FriendlyId
-  friendly_id :headline, use: :scoped, scope: :journal
+    # FriendlyId does the legwork to make the slug uri-friendly
+    extend FriendlyId
+    friendly_id :headline, use: :scoped, scope: :journal
 
-  attribute :published_at, :datetime
+    attribute :published_at, :datetime
 
-  # @!attribute journal
-  #   @return [Journal::Journal]
-  belongs_to :journal, class_name: "Journal::Journal", inverse_of: :entries
-  delegate :room, :space, to: :journal
+    # @!attribute journal
+    #   @return [Journal::Journal]
+    belongs_to :journal, class_name: "Journal::Journal", inverse_of: :entries
+    delegate :room, :space, to: :journal
 
-  def published?
-    published_at.present?
-  end
+    def published?
+      published_at.present?
+    end
 
-  def to_html
-    render_markdown(body)
-  end
+    def to_html
+      render_markdown(body)
+    end
 
-  def to_param
-    slug
-  end
+    def self.renderer
+      @_renderer ||= Redcarpet::Markdown.new(
+        Renderer.new(filter_html: true, with_toc_data: true),
+        autolink: true, strikethrough: true,
+        no_intra_emphasis: true,
+        lax_spacing: true,
+        fenced_code_blocks: true, disable_indented_code_blocks: true,
+        tables: true, footnotes: true, superscript: true, quote: true
+      )
+    end
 
-  def location(action = :show)
-    case action
-    when :new
-      [:new] + journal.location + [:entry]
-    when :edit
-      [:edit] + journal.location + [self]
-    else
-      journal.location + [self]
+    def to_param
+      slug
+    end
+
+    def location(action = :show)
+      case action
+      when :new
+        [:new] + journal.location + [:entry]
+      when :edit
+        [:edit] + journal.location + [self]
+      else
+        journal.location + [self]
+      end
     end
   end
 end
