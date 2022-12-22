68ba2e3084715eb6daa23b2af719303292506849
Zee Spencer <50284+zspencer@users.noreply.github.com>
2022-12-21 21:04:43 -0800

    EmbeddedForm: Drop Placeable

--- spec/factories/furniture/embedded_form.rb
+++ spec/factories/furniture/embedded_form.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :embedded_form do
-    transient do
-      room { build(:room) }
-    end
-
-    placement do
-      association :furniture_placement, {furniture_kind: "embedded_form", room: room}
-    end
-  end
+  factory :embedded_form
 end
