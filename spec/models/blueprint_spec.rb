1d18402a68e01b443d76dafac4e1f2bf6f0a58ef
Ana Ulin <ana@ulin.org>
2022-12-22 19:43:53 -0800

    Update blueprint spec to account for the fact that the example configuration no longer has a utilty hookup.

--- spec/models/blueprint_spec.rb
+++ spec/models/blueprint_spec.rb
@@ -29,15 +29,12 @@ RSpec.describe Blueprint do
 
       # @todo add other examples of changing data after the
       # blueprint has been applied
-      space.utility_hookups.first.update(utility_attributes: {client_id: "1234"})
-
       space.rooms.first.furniture_placements.first.update(furniture_attributes: {content: "Hey there!"})
 
       Blueprint.new(EXAMPLE_CONFIG).find_or_create!
 
       # @todo add other examples of confirming the changes
       # were not overwritten
-      expect(space.utility_hookups.first.utility.client_id).to eql("1234")
       expect(space.rooms.first.furniture_placements.first.furniture.content).to eql("Hey there!")
     end
 
@@ -47,7 +44,6 @@ RSpec.describe Blueprint do
       Blueprint.new(EXAMPLE_CONFIG.merge(space: space)).find_or_create!
 
       expect(space.rooms.count).to be(1)
-      expect(space.utility_hookups.count).to be(1)
     end
   end
 end
