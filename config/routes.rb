9350ee22bbf61e19a7f53e7b6a4c55c242d1c4a2
Zee <50284+zspencer@users.noreply.github.com>
2023-01-24 11:55:16 -0800

    Spaces - BYODomains: Fix broken Space/edit page (#1068)
    https://github.com/zinc-collective/convene/issues/74
    https://github.com/zinc-collective/convene/issues/1067
    
    Turns out, having the Space routes split into two sections meant some of
    the updates to the routes did not get applied for spaces with
    BYO-Domains.
    
    This... fixes that...

--- config/routes.rb
+++ config/routes.rb
@@ -9,21 +9,7 @@ Rails.application.routes.draw do
   # post "/auth/:provider/callback", "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :spaces, only: %I[show edit update create destroy] do
-    resource :authenticated_session, only: %i[new create update destroy show]
-
-    resources :invitations, only: %i[create destroy index] do
-      resource :rsvp, only: %i[show update]
-    end
-
-    resources :rooms, only: %i[show edit update new create destroy] do
-      Furniture.append_routes(self)
-      resource :waiting_room, only: %i[show update]
-      resources :furniture_placements, only: %i[create edit update destroy]
-    end
-
-    resources :utility_hookups
-
-    resources :memberships, only: %I[index show destroy]
+    SpaceRoutes.append_routes(self)
   end
 
   resources :memberships, only: %I[create]
@@ -42,18 +28,6 @@ Rails.application.routes.draw do
     get "/" => "spaces#show"
     put "/" => "spaces#update"
 
-    resources :invitations, only: %i[create destroy index] do
-      resource :rsvp, only: %i[show update]
-    end
-
-    resources :memberships, only: %i[index show destroy]
-
-    resources :utility_hookups, only: %I[create edit update destroy index]
-
-    resource :authenticated_session, only: %i[new create update destroy show]
-
-    resources :rooms, only: %i[show edit update new create destroy] do
-      Furniture.append_routes(self)
-    end
+    SpaceRoutes.append_routes(self)
   end
 end
