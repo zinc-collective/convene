6c84b7f1ca0df554348b0d331e3c83a40885cc3d
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-01-24 20:50:22 -0800

    Get the test to pass

--- spec/furniture/marketplace/stripe_accounts_controller_request_spec.rb
+++ spec/furniture/marketplace/stripe_accounts_controller_request_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe Marketplace::StripeAccountsController, type: :request do
   let(:space) { marketplace.space }
   let(:room) { marketplace.room }
   let(:member) { create(:membership, space: space).member }
-  let!(:stripe) { create(:utility_hookup, :stripe, space: space, stripe_api_key: "asdf") }
-  let(:account_link) { double(Stripe::AccountLink, url: "http://example.com/") }
+  let!(:stripe) { create(:utility_hookup, :stripe, space: space, configuration: { "api_token" => "asdf" }) }
+  let(:stripe_account_link) { double(Stripe::AccountLink, url: "http://example.com/") }
+  let(:stripe_account) { double(Stripe::Account, id: "ac_1234", details_submitted?: false)}
 
   before do
-    allow(Stripe::AccountLink).to receive(:create).and_return(account_link)
+    allow(Stripe::Account).to receive(:create).and_return(stripe_account)
+    allow(Stripe::AccountLink).to receive(:create).and_return(stripe_account_link)
   end
 
   describe "#create" do
@@ -19,7 +21,7 @@ RSpec.describe Marketplace::StripeAccountsController, type: :request do
       response
     end
 
-    it { is_expected.to redirect_to(account_link.url) }
-    # , status: :see_other, allow_other_hosts: true
+    it { is_expected.to redirect_to(stripe_account_link.url) }
+    # Not sure how to test this: status: :see_other, allow_other_hosts: true
   end
 end
