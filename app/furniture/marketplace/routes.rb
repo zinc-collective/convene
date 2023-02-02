42a72266c6eed03f034b7d10e2e635b276d9f107
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-02-01 18:56:39 -0800

    Marketplace: Vendor receives Payment for Products
    OK, this is the first shot across the bow for listening to stripe
    checkout completions and sending the money onwards to Vendors
    
    Co-authored-by: Dalton <daltonrpruitt@users.noreply.github.com>
    Co-authored-by: Ana Ulin <anaulin@users.noreply.github.com>

--- app/furniture/marketplace/routes.rb
+++ app/furniture/marketplace/routes.rb
@@ -2,6 +2,8 @@ class Marketplace
   class Routes
     def self.append_routes(router)
       router.resources :marketplaces, only: [:show, :edit, :update], module: "marketplace" do
+        router.resources :stripe_events
+
         router.resources :carts do
           router.resources :cart_products
           router.resource :checkout, only: [:show, :create]
---
+++ app/furniture/marketplace/stripe_events_controller.rb
@@ -0,0 +1,27 @@
+class Marketplace
+  class StripeEventsController < Controller
+    skip_before_action :verify_authenticity_token
+
+    # @todo add signing check using the stripe webhook signing secret
+    # @see https://stripe.com/docs/webhooks/quickstart
+    def create
+      skip_authorization
+      payload = request.body.read
+      event = Stripe::Event.construct_from(params)
+
+      case event.type
+
+      when "checkout.session.completed"
+        payment_intent = Stripe::PaymentIntent.retrieve(event.data.dig("object", "payment_intent"), {api_key: marketplace.stripe_api_key})
+        order = Order.find(payment_intent.transfer_group)
+
+        transfer = Stripe::Transfer.create({
+          amount: order.price_total.cents,
+          currency: "usd",
+          destination: marketplace.stripe_account,
+          transfer_group: order.id
+        }, {api_key: marketplace.stripe_api_key})
+      end
+    end
+  end
+end
