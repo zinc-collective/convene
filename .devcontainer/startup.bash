7065402632cc01ff474ad8c0f5d471d7bd0d8798
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2023-02-11 21:11:05 +0000

    Try one more time to have bin/run work
    I will remove again if this fails

--- .devcontainer/startup.bash
+++ .devcontainer/startup.bash
@@ -5,7 +5,7 @@ echo "See files in '.devcontainer/output' for errors and other info"
 echo "Startup containers"
 docker compose up &> .devcontainer/output/docker_compose_up.out &
 
-sleep 40
+sleep 30
 
 if [ ! -f .env ]; then 
     cp .env.example .env 
@@ -15,10 +15,12 @@ echo "Run 'bin/setup'"
 bin/setup &> .devcontainer/output/bin_setup.out
 
 rm -f .overmind.sock 
-# echo "Run 'bin/run'"
-# bin/run &> .devcontainer/output/bin_run.out &
-echo "Now you can run 'bin/run' on the command line!"
-echo "Then the app should be running soon!"
+echo "Run 'bin/run'"
+bin/run &> .devcontainer/output/bin_run.out &
+# echo "Now you can run 'bin/run' on the command line!"
+# echo "Then the app should be running soon!"
+
+echo "The app should be running soon!"
 echo "To access the app in your browser:"
 echo "  - Go to 'Ports' tab of the terminal pane"
 echo "  - Wait for Port 3000 to show up in left-hand column"
