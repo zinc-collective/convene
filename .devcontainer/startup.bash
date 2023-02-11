fe43fbb3de49ff8f5646efd1b0d1b898331b62e4
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2023-02-11 20:44:15 +0000

    Don't run bin/run automatically
    Let user know to do it manually, though.

--- .devcontainer/startup.bash
+++ .devcontainer/startup.bash
@@ -15,10 +15,10 @@ echo "Run 'bin/setup'"
 bin/setup &> .devcontainer/output/bin_setup.out
 
 rm -f .overmind.sock 
-echo "Run 'bin/run'"
-bin/run &> .devcontainer/output/bin_run.out &
-
-echo "App should be running soon!"
+# echo "Run 'bin/run'"
+# bin/run &> .devcontainer/output/bin_run.out &
+echo "Now you can run 'bin/run' on the command line!"
+echo "Then the app should be running soon!"
 echo "To access the app in your browser:"
 echo "  - Go to 'Ports' tab of the terminal pane"
 echo "  - Wait for Port 3000 to show up in left-hand column"
