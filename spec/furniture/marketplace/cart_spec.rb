ca70c4711a212a9f4cd088f480fc51c78d8748c4
KellyAH <KellyAH@users.noreply.github.com>
2023-02-23 02:48:24 +0000

    add unit test for Order#product_total
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Dalton Pruitt <daltonrpruitt@users.noreply.github.com>

--- spec/furniture/marketplace/cart_spec.rb
+++ spec/furniture/marketplace/cart_spec.rb
@@ -24,4 +24,19 @@ RSpec.describe Marketplace::Cart, type: :model do
 
     it { is_expected.to eql(product_a.price + product_b.price + product_b.price + marketplace.delivery_fee) }
   end
+
+  describe "#product_total" do
+    subject(:product_total) { cart.product_total }
+
+    let(:cart) { create(:marketplace_cart) }
+    let(:product_a) { create(:marketplace_product, marketplace: cart.marketplace) }
+    let(:product_b) { create(:marketplace_product, marketplace: cart.marketplace) }
+
+    before do
+      cart.cart_products.create!(product: product_a, quantity: 3)
+      cart.cart_products.create!(product: product_b, quantity: 2)
+    end
+
+    it { is_expected.to eql(product_a.price * 3 + product_b.price * 2) }
+  end
 end
