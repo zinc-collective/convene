0727632b7876797fcea76c5b938688194bb81206
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-03-02 12:31:37 -0800

    🔐 `Marketplace`: Encrypt delivery addresses!
    - https://github.com/zinc-collective/convene/issues/1136
    - https://github.com/zinc-collective/convene/issues/831
    
    So, I totally didn't think about how delivery addresses are PII and
    probably should not be stored in plaintext! Womp. Womp. Womp.
    
    Now they ain't!
    
    We'll want to delete the `release:after_build` bits after a prod deploy.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_024315) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_202459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -134,8 +134,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_024315) do
     t.uuid "shopper_id"
     t.string "status", default: "pre_checkout", null: false
     t.string "stripe_session_id"
-    t.string "delivery_address"
+    t.string "deprecated_delivery_address"
     t.string "contact_email"
+    t.text "delivery_address_ciphertext"
     t.index ["marketplace_id"], name: "index_marketplace_orders_on_marketplace_id"
     t.index ["shopper_id"], name: "index_marketplace_orders_on_shopper_id"
   end
--- lib/tasks/release.rake
+++ lib/tasks/release.rake
@@ -3,6 +3,12 @@
 namespace :release do
   desc "Ensures any post-release / pre-deploy behavior has occurred"
   task after_build: [:environment, "db:prepare"] do
+    # @todo Delete after running in prod
+    Marketplace::Order.all.find_each do |order|
+      next unless order.deprecated_delivery_address.present?
+
+      order.update(delivery_address: order.deprecated_delivery_address, deprecated_delivery_address: nil)
+    end
     SystemTestSpace.prepare
   end
 end
