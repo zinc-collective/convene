6c6fec4067fed735cb5ea267bed0a209872004da
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-03-05 17:57:48 -0800

    🛠️ `Component`: Set up `view_component` `ComponentPreview`s
    - https://github.com/zinc-collective/convene/issues/1187
    
    This is an intermediary step before we look into `LookBook` or similar,
    but it:
    
    1. Makes it possible to define component previews in
       `spec/components/previews`
    2. Creates a basic layout for showing Components

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,8 @@ module ConveneWeb
 
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.load_path += Dir[Rails.root.join("app", "furniture", "**", "locales", "**", "*.{rb,yml}")]
+
+    config.view_component.preview_paths << "#{Rails.root}/spec/components/previews"
+    config.view_component.default_preview_layout = "component_preview"
   end
 end
