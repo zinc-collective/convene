019226cedeaf30c131a34ffbdb47f87beb2b5409
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-03-11 06:36:59 -0800

    🧹 `Furniture`: Appease the Fuzz
    - https://github.com/zinc-collective/convene/pull/1209#discussion_r1133016759
    - https://github.com/zinc-collective/convene/issues/709
    
    I don't really like the direction this pushes specs, but I can live with
    it.
    
    In particular, a single assert per request/response seems...
    excruciatingly slow.
    
    But there are other mechanisms we can explore to mitigate that as it
    gets out of hand.

--- spec/furniture/embedded_form_spec.rb
+++ spec/furniture/embedded_form_spec.rb
@@ -3,16 +3,27 @@
 require "rails_helper"
 
 RSpec.describe EmbeddedForm do
+  subject(:embedded_form) { build(:embedded_form, form_url: form_url) }
+
   describe "#embeddable_form_url" do
-    it "converts Airtable Share URLs into the Embed url" do
-      embedded_form = build(:embedded_form, form_url: "https://airtable.com/shrCnrBzflvzDIlvg")
-      expect(embedded_form.embeddable_form_url).to eql("https://airtable.com/embed/shrCnrBzflvzDIlvg")
+    subject(:embeddable_form_url) { embedded_form.embeddable_form_url }
 
-      embedded_form = build(:embedded_form, form_url: "https://airtable.com/shrCnrBzflvzDIlvg/embed")
-      expect(embedded_form.embeddable_form_url).to eql("https://airtable.com/embed/shrCnrBzflvzDIlvg")
+    context "with a #form_url that is the basic share url" do
+      let(:form_url) { "https://airtable.com/shrCnrBzflvzDIlvg" }
 
-      embedded_form = build(:embedded_form, form_url: "https://airtable.com/embed/shrCnrBzflvzDIlvg")
-      expect(embedded_form.embeddable_form_url).to eql("https://airtable.com/embed/shrCnrBzflvzDIlvg")
+      it { is_expected.to eql "https://airtable.com/embed/shrCnrBzflvzDIlvg" }
+    end
+
+    context "with a #form_url that is the embed instructions url" do
+      let(:form_url) { "https://airtable.com/shrCnrBzflvzDIlvg/embed" }
+
+      it { is_expected.to eql("https://airtable.com/embed/shrCnrBzflvzDIlvg") }
+    end
+
+    context "with a #form_url that is the actual embed url" do
+      let(:form_url) { "https://airtable.com/embed/shrCnrBzflvzDIlvg" }
+
+      it { is_expected.to eql("https://airtable.com/embed/shrCnrBzflvzDIlvg") }
     end
   end
 end
--- spec/furniture/journal/entries_request_spec.rb
+++ spec/furniture/journal/entries_request_spec.rb
@@ -7,44 +7,60 @@ RSpec.describe Journal::EntriesController, type: :request do
   let(:room) { journal.room }
   let(:member) { create(:person, spaces: [space]) }
 
+  before { sign_in(space, member) }
+
   describe "#index" do
-    it "shows all the journal entries" do
-      sign_in(space, member)
-      unpublished_entry = create(:journal_entry, journal: journal)
-      published_entry = create(:journal_entry, journal: journal, published_at: 1.week.ago)
-
+    subject(:perform_request) do
       get polymorphic_path(journal.location(child: :entries))
+      response
+    end
 
-      expect(response).to be_ok
-      expect(response.body).to include(CGI.escapeHTML(published_entry.headline))
-      expect(response.body).to include(CGI.escapeHTML(unpublished_entry.headline))
+    let!(:unpublished_entry) { create(:journal_entry, journal: journal) }
+    let!(:published_entry) { create(:journal_entry, journal: journal, published_at: 1.week.ago) }
+
+    it { is_expected.to be_ok }
+
+    describe "response body" do
+      subject(:response_body) { response.body }
+
+      before { perform_request }
+
+      it { is_expected.to include(CGI.escapeHTML(published_entry.headline)) }
+      it { is_expected.to include(CGI.escapeHTML(unpublished_entry.headline)) }
     end
   end
 
   describe "#create" do
-    it "Creates an Entry in the Journal" do
-      sign_in(space, member)
-
-      attributes = attributes_for(:journal_entry)
-
-      expect do
-        post polymorphic_path([space, room, journal, :entries]), params: {entry: attributes}
-      end.to change(journal.entries, :count).by(1)
-
-      created_entry = journal.entries.first
-      expect(created_entry.headline).to eql(attributes[:headline])
-      expect(created_entry.body).to eql(attributes[:body])
+    subject(:perform_request) do
+      post polymorphic_path([space, room, journal, :entries]), params: {entry: journal_entry_attributes}
     end
+
+    let(:journal_entry_attributes) { attributes_for(:journal_entry) }
+    let(:created_journal_entry) { journal.entries.first }
+
+    specify { expect { perform_request }.to change(journal.entries, :count).by(1) }
+
+    specify {
+      perform_request
+      expect(created_journal_entry.headline).to eql(journal_entry_attributes[:headline])
+    }
+
+    specify {
+      perform_request
+      expect(created_journal_entry.body).to eql(journal_entry_attributes[:body])
+    }
   end
 
   describe "#update" do
-    it "allows members to update Journal Entries" do
-      sign_in(space, member)
-      entry = create(:journal_entry, journal: journal)
-      published_at = 1.day.ago.beginning_of_day
-      expect do
-        put polymorphic_path(entry.location), params: {entry: {published_at: published_at}}
-      end.to change { entry.reload.published_at }.from(nil).to(published_at.to_time)
+    subject(:perform_request) do
+      put polymorphic_path(entry.location), params: {entry: {published_at: published_at}}
     end
+
+    let(:published_at) { 1.day.ago.beginning_of_day }
+    let(:entry) { create(:journal_entry, journal: journal) }
+
+    before { sign_in(space, member) }
+
+    specify { expect { perform_request }.to change { entry.reload.published_at }.from(nil).to(published_at.to_time) }
   end
 end
