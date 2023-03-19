4180d01db1337c683feedf1ffb21a442db010405
KellyAH <KellyAH@users.noreply.github.com>
2023-03-19 17:46:02 +0000

    add placed_at to marketplace_orders table to prep for saving the stripe order placed_at date.
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Dalton Pruitt <daltonrpruitt@users.noreply.github.com>
    Co-authored-by: Dicko Sow <s12dsow@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_11_011858) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_173737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_11_011858) do
     t.string "contact_email"
     t.text "delivery_address_ciphertext"
     t.string "contact_phone_number_ciphertext"
+    t.datetime "placed_at"
     t.index ["marketplace_id"], name: "index_marketplace_orders_on_marketplace_id"
     t.index ["shopper_id"], name: "index_marketplace_orders_on_shopper_id"
   end
