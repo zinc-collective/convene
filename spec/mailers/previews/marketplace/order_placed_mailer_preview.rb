0f30e835c55ac88faf4ab86c62757579f0e95373
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-03-24 16:51:58 -0700

    `Marketplace`: Fill out `Order::PlacedMailer` body

--- spec/mailers/previews/marketplace/order_placed_mailer_preview.rb
+++ spec/mailers/previews/marketplace/order_placed_mailer_preview.rb
@@ -12,6 +12,7 @@ class Marketplace
 
       tax_rate = FactoryBot.build(:marketplace_tax_rate, marketplace: marketplace)
       order = FactoryBot.build(:marketplace_order, :with_products, marketplace: marketplace,
+        delivery_window: 1.hour.from_now, placed_at: 5.minutes.ago,
         product_count: (1..5).to_a.sample, delivery_address: Faker::Address.full_address,
         contact_email: Faker::Internet.safe_email, contact_phone_number: Faker::PhoneNumber.cell_phone)
 
