1e607d273c9ed776e3fc803b90af8d318371bc99
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-03-29 17:27:20 -0700

    🐞 `Marketplace`: Fix deleting `Product` with `TaxRate`
    - https://github.com/zinc-collective/convene/issues/1272
    
    Forgot a `dependent: :destroy`

--- spec/furniture/marketplace/product_spec.rb
+++ spec/furniture/marketplace/product_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Marketplace::Product, type: :model do
   it { is_expected.to belong_to(:marketplace).inverse_of(:products) }
   it { is_expected.to have_many(:cart_products).inverse_of(:product).dependent(:destroy) }
   it { is_expected.to have_many(:carts).through(:cart_products).inverse_of(:products) }
+  it { is_expected.to have_many(:product_tax_rates).inverse_of(:product).dependent(:destroy) }
+  it { is_expected.to have_many(:tax_rates).through(:product_tax_rates).inverse_of(:products) }
 
   describe "#name" do
     it { is_expected.to validate_presence_of(:name) }
