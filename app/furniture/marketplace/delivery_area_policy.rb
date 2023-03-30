048bb46a7b884d1c82600b6c8745f50a4144a19a
KellyAH <KellyAH@users.noreply.github.com>
2023-03-30 02:20:00 +0000

    - Display 'Marketplace::DeliveryArea' records on  index view in a card component. - Fix `Marketplace::DeliveryAreasController` to create new DeliveryArea records.
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Dalton Pruitt <daltonrpruitt@users.noreply.github.com>
    Co-authored-by: Ross Chapman <rosschapman@users.noreply.github.com>

--- app/furniture/marketplace/delivery_area_policy.rb
+++ app/furniture/marketplace/delivery_area_policy.rb
@@ -6,6 +6,10 @@ class Marketplace
       person.operator? || person.member_of?(delivery_area.marketplace.space)
     end
 
+    def permitted_attributes(_)
+      [:label, :price]
+    end
+
     class Scope < ApplicationScope
       def resolve
         scope.all
--- app/furniture/marketplace/delivery_areas_controller.rb
+++ app/furniture/marketplace/delivery_areas_controller.rb
@@ -3,12 +3,30 @@ class Marketplace
     def index
     end
 
+    def create
+      if delivery_area.save
+        redirect_to marketplace.location(child: :delivery_areas)
+      else
+        render :new
+      end
+    end
+
     def new
       delivery_area
     end
 
+    def delivery_area_params
+      policy(DeliveryArea).permit(params.require(:delivery_area))
+    end
+
     helper_method def delivery_area
-      @delivery_area ||= authorize(delivery_areas.new)
+      @delivery_area ||= if params[:delivery_area]
+        delivery_areas.new(delivery_area_params)
+      else
+        delivery_areas.new
+      end.tap do |delivery_area|
+        authorize(delivery_area)
+      end
     end
 
     helper_method def delivery_areas
