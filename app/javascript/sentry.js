9dd2bbfaa6212cbdef32d79b8da3adf666a3da88
Ana Ulin <ana@ulin.org>
2023-04-02 16:54:26 -0700

    🛠️ `Neighborhood`: Report browser JavaScritp errors to Sentry (#1296)
    Add Sentry error reporting from the browser.

---
+++ app/javascript/sentry.js
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/browser";
+
+export function initializeSentry() {
+  const dsn = document.head.querySelector("meta[name=sentry_dsn]").content;
+  if (!dsn) {
+    return;
+  }
+
+  Sentry.init({
+    dsn: dsn,
+    integrations: [new Sentry.BrowserTracing()],
+    release: document.head.querySelector("meta[name=release_tag").content,
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
