7086966999aca9b8c0690ae3f7368690c01340fb
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-05 18:13:34 -0700

    `Neighborhood`: Write some tests for the costraint
    Co-authored-by: Kelly Hong <KellyAH@users.noreply.github.com>

--- app/models/operator/route_constraint.rb
+++ app/models/operator/route_constraint.rb
@@ -1,7 +1,8 @@
 class Operator
+  # Restricts a route to Operators
+  # @see https://guides.rubyonrails.org/routing.html#specifying-constraints
   class RouteConstraint
     def matches?(request)
-
       return false unless request.session[:person_id]
       Person.find_by(id: request.session[:person_id])&.operator?
     end
