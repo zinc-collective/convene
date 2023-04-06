0dc5b0521f3881e66591e2bfe0c95f0054ea4858
Zee <50284+zspencer@users.noreply.github.com>
2023-04-05 19:11:03 -0700

    🥗 `Marketplace`: Destroy `DeliveryArea` when destroying `Marketplace` (#1318)
    `Marketplace`: Destroy `DeliveryArea` when destroying `Marketplace`
    
    - https://github.com/zinc-collective/convene/issues/1136
    
    When pairing with @KellyAH, it turned out she couldn't get rid  of
    `Marketplace`s in her local database because our `delivery_areas` stick
    around forever.

--- spec/furniture/marketplace/marketplace_spec.rb
+++ spec/furniture/marketplace/marketplace_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Marketplace::Marketplace, type: :model do
   it { is_expected.to have_many(:products).inverse_of(:marketplace).dependent(:destroy) }
   it { is_expected.to have_many(:orders).inverse_of(:marketplace) }
   it { is_expected.to have_many(:carts).inverse_of(:marketplace).dependent(:destroy) }
+  it { is_expected.to have_many(:delivery_areas).inverse_of(:marketplace).dependent(:destroy) }
+  it { is_expected.to have_many(:tax_rates).inverse_of(:marketplace) }
 
   describe "#destroy" do
     let(:marketplace) { create(:marketplace, :with_orders) }
