25ac24e579c2ca33e5b02bbcdddd765804376471
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-07 22:54:56 -0700

    `Marketplace`: Push the `Cart::Delivery#edit` link to the right

--- app/furniture/marketplace/cart/delivery/edit_button_component.rb
+++ app/furniture/marketplace/cart/delivery/edit_button_component.rb
@@ -4,7 +4,7 @@ class Marketplace
       class EditButtonComponent < ButtonComponent
         attr_accessor :delivery
         def initialize(delivery,
-          classes: "shrink font-light text-xs m-0 bg-primary-200 underline",
+          classes: "w-full sm:w-auto font-light text-xs m-0 bg-primary-200 underline",
           label: t("marketplace.cart.deliveries.edit.link_to"))
           self.delivery = delivery
           super(method: :get, href: delivery.location(:edit), label: label, turbo_stream: true, title: nil, classes: classes)
