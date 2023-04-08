212809670ecea829271a038b1298addf9da0a653
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-07 18:29:43 -0700

    OK

--- db/migrate/20230227232512_add_operator_to_person.rb
+++ db/migrate/20230227232512_add_operator_to_person.rb
@@ -1,5 +1,5 @@
 class AddOperatorToPerson < ActiveRecord::Migration[7.0]
   def change
-    add_column :people, :operator, :boolean, default: false
+    add_column :people, :operator, :boolean, default: false, null: false
   end
 end
