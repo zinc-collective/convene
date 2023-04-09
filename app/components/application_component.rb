6f8eb1c03b5535989328d10631c7e0714c9d2d8a
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-09 15:46:58 -0700

    🛠️`Components`: `ApplicationComponent` supports `dom_id`
    If a `dom_id` is set on the component, it will populate the elements
    `id` field with the `dom_id`.
    
    It it's not, but `dom_id` is called with arguments it will delegate that
    to `ViewComponent::Base.dom_id`

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,19 +1,26 @@
 class ApplicationComponent < ViewComponent::Base
   attr_accessor :data
-  attr_writer :classes
+  attr_writer :classes, :dom_id
 
-  def initialize(data: {}, classes: "")
+  def initialize(data: {}, dom_id: nil, classes: "")
     self.data = data
     self.classes = classes
+    self.dom_id = dom_id
   end
 
   # @todo this should gracefully merge left passed in data
   def attributes(classes: "")
-    tag_builder.tag_options(data: data, class: self.classes(classes))
+    tag_builder.tag_options(id: dom_id, data: data, class: self.classes(classes))
   end
 
   # @todo how do we want to handle when tailwind is given conflicting classes? i.e. `mt-5 mt-3`
   def classes(others = "")
     "#{@classes} #{others}".strip.split.compact.uniq.join(" ")
+  end
+
+  def dom_id(*args, **kwargs)
+    return @dom_id if @dom_id
+    return if args.blank?
+    super
   end
 end
