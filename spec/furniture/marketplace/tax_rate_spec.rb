a8b2c6cc4ec8c2d89e3c44ef94975b0f80aa621a
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-10 14:55:35 -0700

    🥗🛠️ `Marketplace`: Add associations and validations to `TaxRate`
    - https://github.com/zinc-collective/convene/issues/1137
    
    This makes the relationships to `space` a bit easier to access from a
    given tax rate, as well as ensures we have good validations.

---
+++ spec/furniture/marketplace/tax_rate_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Marketplace::TaxRate, type: :model do
+  it { is_expected.to validate_presence_of(:tax_rate) }
+  it { is_expected.to validate_numericality_of(:tax_rate).is_less_than_or_equal_to(100).is_greater_than(0) }
+  it { is_expected.to validate_presence_of(:label) }
+
+  it { is_expected.to have_many(:product_tax_rates).inverse_of(:tax_rate) }
+  it { is_expected.to have_many(:products).through(:product_tax_rates).inverse_of(:tax_rates) }
+  it { is_expected.to have_one(:space).through(:marketplace) }
+  it { is_expected.to belong_to(:marketplace).inverse_of(:tax_rates) }
+end
