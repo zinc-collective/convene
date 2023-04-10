5d31eb6d2fa30411b5ee11c1af356c51b2622e49
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-10 14:30:06 -0700

    🛠️ `Component`s: `spec`s with `type: :component` support `polymorphic_path`
    - https://github.com/zinc-collective/convene/pull/1338/files
    - https://github.com/zinc-collective/convene/issues/1187
    
    There are probably other ways to do this, but this seemed like the most
    straightforward way 🙃

--- spec/support/view_component.rb
+++ spec/support/view_component.rb
@@ -2,7 +2,18 @@
 require "view_component/test_helpers"
 require "capybara/rspec"
 
+module Spec
+  module Support
+    module ViewComponent
+      def polymorphic_path(*args, **kwargs)
+        controller.polymorphic_path(*args, **kwargs)
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
+  config.include Spec::Support::ViewComponent, type: :component
   config.include ViewComponent::TestHelpers, type: :component
   config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
