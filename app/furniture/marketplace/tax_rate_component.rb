d184d0a4b357923e9bfc523c7e7796a834027377
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-10 14:49:31 -0700

    ✍️ `Marketplace`: Wordsmith `TaxRate#edit` and `TaxRate#destroy` buttons
    - https://github.com/zinc-collective/convene/issues/1137
    
    This makes it so the text of the button is "Edit" and the label for the
    button is the fuller text which includes the kind of thing being edited.

--- app/furniture/marketplace/tax_rate_component.rb
+++ app/furniture/marketplace/tax_rate_component.rb
@@ -16,7 +16,7 @@ class Marketplace
     def edit_button
       return unless edit_button?
 
-      ButtonComponent.new label: "#{t("icons.edit")} #{t("marketplace.tax_rates.edit.link_to", name: tax_rate.label)}",
+      ButtonComponent.new label: "#{t("icons.edit")} #{t("edit.link_to")}",
         title: t("marketplace.tax_rates.edit.link_to", name: tax_rate.label),
         href: tax_rate.location(:edit), turbo_stream: true,
         method: :get
@@ -29,7 +29,7 @@ class Marketplace
     def destroy_button
       return unless destroy_button?
 
-      ButtonComponent.new label: "#{t("icons.destroy")} #{t("marketplace.tax_rates.destroy.link_to", name: tax_rate.label)}",
+      ButtonComponent.new label: "#{t("icons.destroy")} #{t("destroy.link_to")}",
         title: t("marketplace.tax_rates.destroy.link_to", name: tax_rate.label),
         href: tax_rate.location, turbo_stream: true,
         method: :delete
