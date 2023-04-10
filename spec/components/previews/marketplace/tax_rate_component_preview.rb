dd094669aa314da9b4c03f271866de2f4db833d8
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-09 18:11:36 -0700

    🌸🥗🛠️✨ `Marketplace`: Tidy up managing `TaxRate`s
    - https://github.com/zinc-collective/convene/issues/1137
    - https://github.com/zinc-collective/convene/issues/1187
    
    OK this is a bit bigger than it probably should be but I came in like a
    wreeeckiin' ball and:
    
    - 🛠️ `Components`: `ApplicationComponent` exposes `policy` and `current_person`
    - 🛠️ `Components`: Component Specs have `polymorphic_path`
    - 🥗 `Marketplace`: Tested `TaxRate`, `TaxRateComponent` and `TaxRatesController`
    - 🌸 `Marketplace`: `TaxRate#edit`, `TaxRate#update`, and
      `TaxRate#destroy` via TurboStreams
    - 🌸 `Marketplace`: `TaxRate#index` and `TaxRate#show` are prettier

--- spec/components/previews/marketplace/tax_rate_component_preview.rb
+++ spec/components/previews/marketplace/tax_rate_component_preview.rb
@@ -1,7 +1,9 @@
 class Marketplace
   class TaxRateComponentPreview < ViewComponent::Preview
-    def card
-      render(TaxRateComponent.new(tax_rate: TaxRate.all.sample))
+    # @param label
+    # @param tax_rate
+    def card(label: "Sales Tax", tax_rate: 8.5)
+      render(TaxRateComponent.new(tax_rate: TaxRate.new(label: label, tax_rate: tax_rate)))
     end
   end
 end
---
+++ spec/furniture/marketplace/tax_rate_component_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe Marketplace::TaxRateComponent, type: :component do
+  let(:component) { described_class.new(tax_rate: tax_rate) }
+  let(:tax_rate) { create(:marketplace_tax_rate) }
+
+  describe "#render" do
+    subject(:output) { render_inline(component) }
+
+    it { is_expected.to have_content(tax_rate.label) }
+    it { is_expected.to have_content(component.helpers.number_to_percentage(tax_rate.tax_rate, precision: 2)) }
+
+    context "when current person is null" do
+      it { is_expected.not_to have_selector("a") }
+    end
+
+    context "when current person is a space member" do
+      before { component.current_person = create(:membership, space: tax_rate.space).member }
+
+      it { is_expected.to have_link(href: polymorphic_path(tax_rate.location(:edit))) }
+      it { is_expected.to have_selector("a[data-method='delete'][data-turbo-stream=true][href='#{polymorphic_path(tax_rate.location)}']") }
+    end
+  end
+end
