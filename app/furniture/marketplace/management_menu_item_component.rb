d5bb517a9a4dbecb167df994694e56af76e5a987
Ana Ulin <ana@ulin.org>
2023-04-11 19:29:51 -0700

    Make management menu responsive: hide text and reduce padding on small screens.

--- app/furniture/marketplace/management_menu_item_component.rb
+++ app/furniture/marketplace/management_menu_item_component.rb
@@ -1,7 +1,7 @@
 class Marketplace
   class ManagementMenuItemComponent < ApplicationComponent
     renders_one :icon, ->(variant: SvgComponent) do
-      variant.new(classes: "w-6 h-6")
+      variant.new(classes: "w-6 h-6 flex-none")
     end
 
     attr_accessor :location
