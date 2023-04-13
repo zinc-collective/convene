51bad18b3e3ca38743165c725e1c0c2a22fd8fb1
KellyAH <KellyAH@users.noreply.github.com>
2023-04-13 01:15:55 +0000

    WIP - add delivery_area_component
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Ross Chapman <rosschapman@users.noreply.github.com>

---
+++ app/furniture/marketplace/delivery_area_component.rb
@@ -0,0 +1,42 @@
+class Marketplace
+  class DeliveryAreaComponent < ApplicationComponent
+    attr_accessor :delivery_area
+    delegate :label, to: :delivery_area
+
+    def initialize(delivery_area:, data: {}, classes: "")
+      super(data: data, classes: classes)
+
+      self.delivery_area = delivery_area
+    end
+
+    # def rate
+    #   number_to_percentage(tax_rate.tax_rate, precision: 2)
+    # end
+
+    def edit_button
+      return unless edit_button?
+
+      ButtonComponent.new label: "#{t("icons.edit")} #{t("edit.link_to")}",
+        title: t("marketplace.delivery_areas.edit.link_to", name: delivery_area.label),
+        href: delivery_area.location(:edit), turbo_stream: true,
+        method: :get
+    end
+
+    def edit_button?
+      delivery_area.persisted? && policy(delivery_area).edit?
+    end
+
+    def destroy_button
+      return unless destroy_button?
+
+      ButtonComponent.new label: "#{t("icons.destroy")} #{t("destroy.link_to")}",
+        title: t("marketplace.delivery_areas.destroy.link_to", name: delivery_area.label),
+        href: delivery_area.location, turbo_stream: true,
+        method: :delete
+    end
+
+    def destroy_button?
+      delivery_area.persisted? && policy(delivery_area).destroy? && delivery_area.products.blank?
+    end
+  end
+end
