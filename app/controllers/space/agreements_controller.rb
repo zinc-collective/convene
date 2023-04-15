f72863615ee123a86ea36e1c101b4964ecccf1f8
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-15 15:28:09 -0700

    🥗✨ `Agreement`: Test and implement the `#create` endpoint
    - https://github.com/zinc-collective/convene/issues/1364
    
    This is only for the classic HTML Request <=> Response Cycle. I realized
    I probably want to do the `#index` Routes before `#create`, or at least
    get started on the bits to expose adding an `Agreement` in the
    `Spaces#edit` page.

--- app/controllers/space/agreements_controller.rb
+++ app/controllers/space/agreements_controller.rb
@@ -1,11 +1,28 @@
 class Space
   class AgreementsController < ApplicationController
     def show
-      authorize(agreement)
+    end
+
+    def create
+      agreement.save
+
+      if agreement.errors.present?
+        render :new, status: :unprocessable_entity
+      else
+        redirect_to space.location(:edit), notice: t(".success", name: agreement.name)
+      end
     end
 
     helper_method def agreement
-      @agreement ||= policy_scope(space.agreements).friendly.find(params[:id])
+      @agreement ||= if params[:id]
+        policy_scope(space.agreements).friendly.find(params[:id])
+      else
+        space.agreements.new(agreement_params)
+      end.tap { |agreement| authorize(agreement) }
+    end
+
+    def agreement_params
+      policy(Agreement).permit(params.require(:agreement))
     end
 
     def space
