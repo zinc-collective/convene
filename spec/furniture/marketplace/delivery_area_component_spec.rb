ba30c6cc37d7ea0183432443b9123dc9f3dbeba0
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-04-16 11:22:34 -0700

    🥗🥔✨ `Marketplace`: Show `DeliveryArea`'s `#order_by` and `#delivery_window`
    - https://github.com/zinc-collective/convene/issues/1185
    
    We added some tests to the `DeliveryAreaComponent` to make sure it looks
    reasonable in each use-case:
    
    - `#delivery_window` with no data
    - `#delivery window` with a string
    - `#order_by` with no data
    - `#order_by` with a string
    
    We also included "by" in the `_window.html.erb` partial so that people
    don't have to write it in their `order_by`.
    
    Co-authored-by: Neer Malathapa <nirmalathapa@users.noreply.github.com>
    Co-authored-by: Dicko Sow <s12dsow@users.noreply.github.com>

---
+++ spec/furniture/marketplace/delivery_area_component_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Marketplace::DeliveryAreaComponent, type: :component do
+  subject(:output) { render_inline(component) }
+
+  let(:operator) { create(:person, operator: true) }
+
+  let(:component) { described_class.new(delivery_area: delivery_area, current_person: operator) }
+  let(:delivery_area) { create(:marketplace_delivery_area) }
+
+  it { is_expected.to have_content(delivery_area.label) }
+  it { is_expected.to have_content(controller.view_context.humanized_money_with_symbol(delivery_area.price)) }
+
+  it { is_expected.to have_selector("a[href='#{polymorphic_path(delivery_area.location)}'][data-turbo=true][data-turbo-stream=true][data-turbo-method=delete][data-method=delete][data-confirm='#{I18n.t("destroy.confirm")}']") }
+  it { is_expected.to have_selector("a[href='#{polymorphic_path(delivery_area.location(:edit))}'][data-turbo=true][data-turbo-method=get][data-turbo-stream=true]") }
+
+  context "when `#delivery_window` is empty" do
+    it { is_expected.to have_content "at your chosen time" }
+  end
+
+  context "when `#delivery_window`` is a string" do
+    let(:delivery_area) { create(:marketplace_delivery_area, delivery_window: "dinnertime same day") }
+
+    it { is_expected.to have_content "for #{delivery_area.delivery_window}" }
+  end
+
+  context "when #order_by is blank" do
+    it { is_expected.not_to have_content "Place orders by" }
+  end
+
+  context "when `#order_by` is set" do
+    let(:delivery_area) { create(:marketplace_delivery_area, order_by: "noon") }
+
+    it { is_expected.to have_content "by #{delivery_area.order_by}" }
+  end
+end
