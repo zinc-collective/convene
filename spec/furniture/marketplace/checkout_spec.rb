499c1a5eb7d10203766e5ff8e46bf2abea2c7943
Ana Ulin <ana@ulin.org>
2023-05-04 12:11:27 -0700

    Tighten up the Marketplace::Checkout spec.

--- spec/furniture/marketplace/checkout_spec.rb
+++ spec/furniture/marketplace/checkout_spec.rb
@@ -1,24 +1,26 @@
 require "rails_helper"
 
 RSpec.describe Marketplace::Checkout do
+  subject(:checkout) { described_class.new(cart: cart) }
+
   describe "#create_stripe_session" do
-    it "creates a line item for the delivery fee" do
+    let(:delivery_area) { create(:marketplace_delivery_area) }
+    let(:cart) { build(:marketplace_cart, delivery_area: delivery_area, marketplace: delivery_area.marketplace) }
+
+    before do
       allow(Stripe::Checkout::Session).to receive(:create)
-      delivery_area = create(:marketplace_delivery_area)
-      cart = build(:marketplace_cart, delivery_area: delivery_area, marketplace: delivery_area.marketplace)
-      checkout = described_class.new(cart: cart)
-
       allow(cart.marketplace).to receive(:stripe_api_key).and_return("FAKE_KEY_1234")
+    end
+
+    it "creates a line item for the delivery fee" do
       checkout.create_stripe_session(success_url: "", cancel_url: "")
-
-      # @todo there is probably some way to actually use the `hash_including` and `array_including` matchers
-      # But I couldnt' figure that out as easily as... uhhh.. reaching deep into the internals of rspec to get the stub
-      # proxy.
-      arguments = RSpec::Mocks.space.proxy_for(Stripe::Checkout::Session).messages_arg_list[0][0]
-
-      expect(arguments[:line_items]).to include(quantity: 1,
-        price_data: {currency: "USD", unit_amount: cart.delivery_area.price.cents,
-                     product_data: {name: "Delivery"}})
+      expect(Stripe::Checkout::Session).to have_received(:create) do |arguments|
+        expect(arguments[:line_items]).to include(
+          quantity: 1,
+          price_data: {currency: "USD", unit_amount: cart.delivery_area.price.cents,
+                       product_data: {name: "Delivery"}}
+        )
+      end
     end
   end
 end
