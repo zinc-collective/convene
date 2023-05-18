6699153923209f7188b6091808603a5022ffefb3
Ana Ulin <ana@ulin.org>
2023-05-17 18:32:37 -0700

    Add Footer element to Card Component.

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,2 +1,16 @@
 class CardComponent < ApplicationComponent
+  renders_one :footer
+
+  private
+
+  def card_classes
+    [
+      "shadow",
+      "rounded-lg",
+      "bg-white",
+      "p-4",
+      "sm:p-6",
+      ("rounded-b-none" if footer?)
+    ].compact.join(" ")
+  end
 end
