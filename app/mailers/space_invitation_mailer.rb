9ec2b6153f9cb9c78505a72f474c9f81946588e5
KellyAH <KellyAH@users.noreply.github.com>
2023-05-20 01:53:48 +0000

    add Space name to the Space Invitation subject line

--- app/mailers/space_invitation_mailer.rb
+++ app/mailers/space_invitation_mailer.rb
@@ -1,6 +1,7 @@
 class SpaceInvitationMailer < ApplicationMailer
-  def space_invitation_email(invitation)
+  def space_invitation_email(invitation, space)
     @invitation = invitation
-    mail(to: @invitation.email)
+    @space = space
+    mail(to: @invitation.email, subject: "You are invited to #{@space}")
   end
 end
