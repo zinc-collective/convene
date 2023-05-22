ab351ec85abdf894fe98cad48c46713fd9a0406d
KellyAH <KellyAH@users.noreply.github.com>
2023-05-22 03:01:20 +0000

    indent

--- app/mailers/space_invitation_mailer.rb
+++ app/mailers/space_invitation_mailer.rb
@@ -2,6 +2,10 @@ class SpaceInvitationMailer < ApplicationMailer
   def space_invitation_email(invitation)
     @invitation = invitation
     @space = invitation.space
-    mail(to: @invitation.email, from: "#{@invitation.invitor_display_name} (via #{@space})", subject: "#{@invitation.invitor_display_name} invited you to #{@space}")
+    mail(
+      to: @invitation.email,
+      from: "#{@invitation.invitor_display_name} (via #{@space})",
+      subject: "#{@invitation.invitor_display_name} invited you to #{@space}"
+    )
   end
 end
