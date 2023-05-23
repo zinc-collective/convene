5316a6bb304497bf009766ca8a30ea1a7b96f7aa
KellyAH <KellyAH@users.noreply.github.com>
2023-05-23 11:37:24 -0700

    🌸✨ `Members`: `Invitation` email conveys which `Space` and `Person` it came from (#1483)
    * add Space name to the Space Invitation subject line
    
    * fix unit test
    
    * update Invitation Email from text with invitor name
    
    * refactor space_invitation_email message and logic
    
    * revert schema to main
    
    * indent
    
    * fix from text and ending salutation
    
    * remove "Space" from email body text
    
    * tweak email body text
    
    * `Invitation`: Update the spec to reflect the email subject change (#1486)
    
    We currently look for the RSVP Link in our system tests by matching
    against the subject line of the email. Which works! However, we don't do
    much with our system tests so it's easy to forget.
    
    ---------
    
    Co-authored-by: Zee <50284+zspencer@users.noreply.github.com>

--- app/mailers/space_invitation_mailer.rb
+++ app/mailers/space_invitation_mailer.rb
@@ -1,6 +1,11 @@
 class SpaceInvitationMailer < ApplicationMailer
   def space_invitation_email(invitation)
     @invitation = invitation
-    mail(to: @invitation.email)
+    @space = invitation.space
+    mail(
+      to: @invitation.email,
+      from: "#{@invitation.invitor_display_name} (via #{@space.name})",
+      subject: "#{@invitation.invitor_display_name} invited you to #{@space.name}"
+    )
   end
 end
