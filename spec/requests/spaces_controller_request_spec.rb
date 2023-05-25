66b9fbff89797a73ba20d4872e7e4a53c068ae53
KellyAH <KellyAH@users.noreply.github.com>
2023-05-24 17:59:41 -0700

    ✨ `Space` add "Website Settings" section and enforce ssl checkbox (#1484)
    * add Space name to the Space Invitation subject line
    
    * fix unit test
    
    * update Invitation Email from text with invitor name
    
    * add enforce ssl checkbox to Space Utilities section
    
    * revert work done for 1483 to main
    
    * add operator only section for enforce ssl checkbox
    
    * gate operator only settings section display by operator
    
    * rename operator settings to website settings
    
    * add WIP unit tests
    
    * add todo
    
    * Enforce SSL checkbox positioned left of label text
    
    * fill out unit tests
    
    * Enforce SSL checkbox value in params updates Space
    
    * remove todo
    
    * `Space` Website Settings is accessable to space members.
    and fix unit tests.
    
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Ana <anaulin@users.noreply.github.com>
    Co-authored-by: Dalton Pruitt <daltonrpruitt@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Ana <anaulin@users.noreply.github.com>
    Co-authored-by: Dalton Pruitt <daltonrpruitt@users.noreply.github.com>

--- spec/requests/spaces_controller_request_spec.rb
+++ spec/requests/spaces_controller_request_spec.rb
@@ -118,9 +118,10 @@ RSpec.describe SpacesController do
 
       it "updates the Space" do
         new_entrance = space.rooms.sample
-        put polymorphic_path(space), params: {space: {entrance_id: new_entrance.id}}
+        put polymorphic_path(space), params: {space: {entrance_id: new_entrance.id, enforce_ssl: 1}}
 
         expect(space.reload.entrance).to eql(new_entrance)
+        expect(space.reload.enforce_ssl).to be true
         expect(flash[:notice]).to include("successfully updated")
       end
     end
