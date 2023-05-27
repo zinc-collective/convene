6843ccdb1549a22fde4ec21af373acf07bf7b1f5
Zee <50284+zspencer@users.noreply.github.com>
2023-05-26 19:45:39 -0700

    🔨🐞 `Invitations`: are deliverable again since they come from a valid Email Address (#1516)
    `Invitations`: Include Neighborhood Support Email as Invitation
    
    - https://github.com/zinc-collective/convene/issues/1515
    
    Turns out, when I had suggested we drop the Convene bits from the email
    I did not think about what that would mean from the email address
    perspective.
    
    While I don't have a Github issue yet for Space's being able to send
    email from their domain; I think that would be a good thing for us to
    support at some point so our clients branding stays front and center and
    we stay in the background.
    
    In the meantime, since it's coming from a convene domain, we should
    probably include the convene support name as well.

--- app/mailers/space_invitation_mailer.rb
+++ app/mailers/space_invitation_mailer.rb
@@ -4,7 +4,7 @@ class SpaceInvitationMailer < ApplicationMailer
     @space = invitation.space
     mail(
       to: @invitation.email,
-      from: "#{@invitation.invitor_display_name} (via #{@space.name})",
+      from: "#{@invitation.invitor_display_name} (via #{@space.name}) #{ENV.fetch("EMAIL_DEFAULT_FROM")}",
       subject: "#{@invitation.invitor_display_name} invited you to #{@space.name}"
     )
   end
