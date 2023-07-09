f1ccf92ec5b2849dfd8c0fc65796f796298f4c7b
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-07-09 10:48:32 -0700

    `Marketplace`: Add success?pass to `Onboarding`

--- spec/furniture/marketplace/onboarding_component_spec.rb
+++ spec/furniture/marketplace/onboarding_component_spec.rb
@@ -17,9 +17,30 @@ RSpec.describe Marketplace::OnboardingComponent, type: :component do
   it { is_expected.to have_content(t(".missing.notification_methods", link: t("marketplace.notification_methods.new.link_to"))) }
   it { is_expected.to have_link(t("marketplace.notification_methods.new.link_to"), href: polymorphic_path(marketplace.location(:new, child: :notification_method))) }
 
+  context "when the Marketplace has a NotificationMethod" do
+    let(:marketplace) { create(:marketplace, :with_notification_methods) }
+
+    it { is_expected.not_to have_content(t(".missing.notification_methods", link: t("marketplace.notification_methods.new.link_to"))) }
+    it { is_expected.not_to have_link(t("marketplace.notification_methods.new.link_to"), href: polymorphic_path(marketplace.location(:new, child: :notification_method))) }
+  end
+
   it { is_expected.to have_content(t(".missing.products", link: t("marketplace.products.new.link_to"))) }
   it { is_expected.to have_link(t("marketplace.products.new.link_to"), href: polymorphic_path(marketplace.location(:new, child: :product))) }
 
+  context "when the Marketplace has a Product" do
+    let(:marketplace) { create(:marketplace, :with_products) }
+
+    it { is_expected.not_to have_content(t(".missing.products", link: t("marketplace.products.new.link_to"))) }
+    it { is_expected.not_to have_link(t("marketplace.products.new.link_to"), href: polymorphic_path(marketplace.location(:new, child: :product))) }
+  end
+
   it { is_expected.to have_content(t(".missing.delivery_areas", link: t("marketplace.delivery_areas.new.link_to"))) }
   it { is_expected.to have_link(t("marketplace.delivery_areas.new.link_to"), href: polymorphic_path(marketplace.location(:new, child: :delivery_area))) }
+
+  context "when the Marketplace has a DeliveryArea" do
+    let(:marketplace) { create(:marketplace, :with_delivery_areas) }
+
+    it { is_expected.not_to have_content(t(".missing.delivery_areas", link: t("marketplace.delivery_areas.new.link_to"))) }
+    it { is_expected.not_to have_link(t("marketplace.delivery_areas.new.link_to"), href: polymorphic_path(marketplace.location(:new, child: :delivery_area))) }
+  end
 end
