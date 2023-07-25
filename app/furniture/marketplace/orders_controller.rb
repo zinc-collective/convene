9b37109837f2eee2002ed62564c3b925ad1f5daf
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-07-25 13:44:29 -0700

    🐞🔨 `Marketplace`: Only list `paid` `Orders`
    - https://github.com/zinc-collective/convene/issues/1691
    
    This just makes sure the orders index isn't overwhelming with all the
    un-checked-out-carts

--- app/furniture/marketplace/orders_controller.rb
+++ app/furniture/marketplace/orders_controller.rb
@@ -1,7 +1,7 @@
 class Marketplace
   class OrdersController < Controller
     expose :order, scope: -> { orders }, model: Order
-    expose :orders, -> { OrderPolicy::Scope.new(shopper, marketplace.orders).resolve.order(created_at: :desc) }
+    expose :orders, -> { OrderPolicy::Scope.new(shopper, marketplace.orders).resolve.paid.order(created_at: :desc) }
 
     def show
       authorize(order)
