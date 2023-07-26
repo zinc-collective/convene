09f5458581a9fc7e1c907c3c695cace516e82b56
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-07-26 12:47:00 -0700

    `Neighborhood`: Use a default `TimeZone` everywhere
    - https://github.com/zinc-collective/convene/issues/1660
    
    This is a small step towards a better way for handling time; since we
    currently only operate out of the SF Bay Area; it seems like a
    reasonable place to start.
    
    At some point we'll want to add in time zone support for browsers,
    users, spaces, etc. but this gets us to a place where things are a
    little better sooner rather than later

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   DEFAULT_FROM = ENV.fetch("EMAIL_DEFAULT_FROM", "convene-support@example.com")
+  include Neighborhood::TimeZoneMixin
 
   default from: DEFAULT_FROM
   prepend_view_path "app/furniture"
---
+++ app/models/neighborhood/time_zone_mixin.rb
@@ -0,0 +1,14 @@
+class Neighborhood
+  module TimeZoneMixin
+    # This is probably not the end-result we're going for; but it is a start
+    # We will probably want to fall-back to the current_person, then browser, then current_space time zones once we
+    # gain access to them.
+    def set_time_zone(&block)
+      Time.use_zone(ENV.fetch("NEIGHBORHOOD_TIME_ZONE"), &block)
+    end
+
+    def self.included(controller_or_job)
+      controller_or_job.around_action :set_time_zone
+    end
+  end
+end
