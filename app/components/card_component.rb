ab9b3ea1def3aedd1a7b5e7553fb5e0bae84225f
Ross Chapman <rosschapman@users.noreply.github.com>
2023-07-26 17:14:21 -0700

    Updates CardComponent layout

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -3,14 +3,27 @@ class CardComponent < ApplicationComponent
 
   private
 
-  def card_classes
+  def card_classes_content
+    [
+      "p-4",
+      "sm:p-6"
+    ].compact.join(" ")
+  end
+
+  def card_classes_wrapper
     [
       "shadow",
       "rounded-lg",
-      "bg-white",
+      "bg-white"
+    ].compact.join(" ")
+  end
+
+  def card_classes_footer
+    [
+      "bg-purple-50",
       "p-4",
       "sm:p-6",
-      ("rounded-b-none" if footer?)
+      ("rounded-t-none" unless content.blank?)
     ].compact.join(" ")
   end
 end
