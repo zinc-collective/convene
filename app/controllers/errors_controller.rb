8a5a0f855e1aac03ef0a2cc776de6ae572626c6f
Ana Ulin <ana@ulin.org>
2023-07-26 19:34:26 -0700

    Replace the standard Rails error pages with a controller that can render custom error pages with the Convene layout.

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class ErrorsController < ApplicationController
+  before_action :skip_authorization
+
+  def show
+    render error_template, status: status_code
+  end
+
+  private
+
+  def status_code
+    @status_code ||=
+      begin
+        request.env["action_dispatch.exception"].try(:status_code) ||
+          ActionDispatch::ExceptionWrapper.status_code_for_exception(request.env["action_dispatch.exception"])
+      rescue => err
+        Sentry.capture_exception(err)
+        500
+      end
+  end
+
+  def error_template
+    (status_code == 404) ? "not_found" : "generic_error"
+  end
+end
