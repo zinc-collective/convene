57beb0d7af66610288a5b4bb5d818e007e30130a
Zee <50284+zspencer@users.noreply.github.com>
2023-07-26 17:04:34 -0700

    🐞🔨 `Marketplace`: Only list `paid` `Orders` (#1703)
    - https://github.com/zinc-collective/convene/issues/1691
    
    This just makes sure the orders index isn't overwhelming with all the
    un-checked-out-carts
    
    * Pull the filtering out to the view for now

--- spec/furniture/marketplace/orders_controller_request_spec.rb
+++ spec/furniture/marketplace/orders_controller_request_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Marketplace::OrdersController, type: :request do
   let(:marketplace) { create(:marketplace, space: space) }
   let(:order) { create(:marketplace_order, marketplace: marketplace, delivery_address: "123 N West St") }
 
+  describe "#index" do
+    it "only includes paid orders by default" do
+      sign_in(space, member)
+      create(:marketplace_order, marketplace: marketplace, delivery_address: "123 W Unpaid St", status: :pre_checkout)
+      create(:marketplace_order, marketplace: marketplace, delivery_address: "123 N Paid St")
+      get polymorphic_path(marketplace.location(child: :orders))
+
+      expect(response.body).to include("123 N Paid St")
+      expect(response.body).not_to include("123 W Unpaid St")
+    end
+  end
+
   describe "#show" do
     subject(:perform_request) do
       get polymorphic_path(order.location)
