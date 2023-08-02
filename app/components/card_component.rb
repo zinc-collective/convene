cc72c812da71db5d3366920a0ee4a3afc07ce3b2
Ross Chapman <rosschapman@users.noreply.github.com>
2023-08-02 15:39:00 -0700

    🌸`Components`: Remove blank space from `Cards` without `content` (#1709)

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -3,14 +3,28 @@ class CardComponent < ApplicationComponent
 
   private
 
-  def card_classes
+  def card_classes_content
+    [
+      "p-4",
+      "sm:p-6"
+    ].compact.join(" ")
+  end
+
+  def card_classes_wrapper
     [
       "shadow",
       "rounded-lg",
-      "bg-white",
+      "bg-white"
+    ].compact.join(" ")
+  end
+
+  def card_classes_footer
+    [
+      "bg-purple-50",
       "p-4",
       "sm:p-6",
-      ("rounded-b-none" if footer?)
+      # content? is not always working as described, and is returning a proc in some cases rather than a boolean
+      ("rounded-t-none" if content.blank?)
     ].compact.join(" ")
   end
 end
