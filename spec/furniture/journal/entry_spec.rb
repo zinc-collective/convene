8868378118ab01c5ab64049e6d0ab74acfa26ea6
Zee <50284+zspencer@users.noreply.github.com>
2023-08-02 18:36:21 -0700

    ✨ `Journal`: `Entry` can be migrated to another `Journal` (#1716)
    `Journal`: `Entry` can be migrated to another `Journal`
    
    - https://github.com/zinc-collective/convene/issues/1566
    
    Also part of the writing I'm doing on https://slipvector.games, I have a
    Journal for non-canon (well, several; depending on if it's worldbuilding
    or concept art or a story) and then a `Journal` for canon content.
    
    I want to be able to take an entry and move it into the Canon journal;
    and I'm fine doing it from the rails console for now; but I probably
    will expose a command end-point like
    `/journal/:entry_id/migrate_to?journal_id=...&keywords[]=AKeyword&keywords[]=BKeyword`
    and add a button on the `Entry#edit` screen to enter the flow for
    selecting the Journal / adding Keywords.

--- spec/furniture/journal/entry_spec.rb
+++ spec/furniture/journal/entry_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe Journal::Entry, type: :model do
   it { is_expected.to have_one(:room).through(:journal) }
   it { is_expected.to have_one(:space).through(:journal) }
 
+  describe "#migrate_to(journal:, keywords: [])" do
+    it "appends the given keywords to body and swaps the different journal" do
+      entry = create(:journal_entry)
+      destination_journal = create(:journal)
+
+      entry.migrate_to(journal: destination_journal, keywords: ["Cooking", "Recipes"])
+      expect(entry.journal).to eql(destination_journal)
+      expect(entry.keywords).to include(destination_journal.keywords.find_by(canonical_keyword: "Recipes"))
+      expect(entry.keywords).to include(destination_journal.keywords.find_by(canonical_keyword: "Cooking"))
+    end
+  end
+
   describe "#save" do
     let(:entry) { create(:journal_entry, body: "#GoodTimes") }
     let(:journal) { entry.journal }
