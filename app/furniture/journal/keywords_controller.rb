9e2b759a761c04fa6a64ca88ba18c2f9813d4462
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-08-14 10:32:51 -0700

    `Journal`: `Keywords` are case insensitive and aliasable

--- app/furniture/journal/keywords_controller.rb
+++ app/furniture/journal/keywords_controller.rb
@@ -1,7 +1,10 @@
 class Journal
   class KeywordsController < FurnitureController
     expose(:keyword, scope: -> { policy_scope(journal.keywords) }, model: Keyword,
-      find: ->(id, scope) { scope.find_by(canonical_keyword: id) })
+      find: lambda do |id, scope|
+        scope.search(id)&.first ||
+          (raise ActiveRecord::RecordNotFound.new(nil, self, id, id))
+      end)
 
     expose(:journal, -> { Journal.find(params[:journal_id]) })
     def show
--- spec/furniture/journal/keywords_controller_request_spec.rb
+++ spec/furniture/journal/keywords_controller_request_spec.rb
@@ -8,7 +8,26 @@ RSpec.describe Journal::KeywordsController, type: :request do
     end
 
     let(:keyword) { create(:journal_keyword) }
+    let(:journal) { keyword.journal }
 
     it { is_expected.to be_ok }
+
+    context "when the keyword is of a different case" do
+      subject(:perform_request) do
+        get polymorphic_path(journal.location(child: :keyword), id: keyword.canonical_keyword.upcase)
+        response
+      end
+
+      it { is_expected.to be_ok }
+    end
+
+    context "when the keyword doesn't exist" do
+      subject(:perform_request) do
+        get polymorphic_path(journal.location(child: :keyword), id: "Gibberish")
+        response
+      end
+
+      it { is_expected.to be_not_found }
+    end
   end
 end
