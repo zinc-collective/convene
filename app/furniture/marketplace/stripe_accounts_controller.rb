b638b5d0f28cd053d3744b90dfc34112a793592c
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-09-15 10:42:24 -0700

    Apply feedback from @anaulin and @rosschapman

--- app/furniture/marketplace/stripe_accounts_controller.rb
+++ app/furniture/marketplace/stripe_accounts_controller.rb
@@ -1,7 +1,6 @@
 class Marketplace
   class StripeAccountsController < Controller
     def create
-      # TODO: Should we be using https://stripe.com/docs/connect/oauth-express-accounts instead?!
       authorize(marketplace, :edit?)
       stripe_account_link = marketplace.stripe_account_link(
         refresh_url: polymorphic_url(marketplace.location(:edit)),
--- spec/furniture/marketplace/collecting_payments_system_spec.rb
+++ spec/furniture/marketplace/collecting_payments_system_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 describe "Marketplace: Collecting Payments", type: :system do
   # @see https://github.com/zinc-collective/convene/issues/1622
   describe "via Stripe" do
-    it "Connecting a Vendor Stripe Account" do
+    it "connects the Vendor's Stripe Account" do
       space = create(:space, :with_entrance, :with_members)
       marketplace = create(:marketplace, :with_stripe_utility, room: space.entrance)
       sign_in(space.members.first, space)
@@ -17,11 +17,7 @@ describe "Marketplace: Collecting Payments", type: :system do
       click_on("Payment Settings")
       click_on("View Stripe Account")
       expect(page).to have_content("Connect to Stripe")
+      # @todo actually figure out how to do the connect to stripe bit :X
     end
-  end
-
-  def url_options
-    server_uri = URI(page.server_url)
-    {host: server_uri.host, port: server_uri.port}
   end
 end
