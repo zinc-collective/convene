6e7bf2217d4613de2b2ce03a231f136aad085386
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-10-09 19:59:04 -0700

    WTF

--- features/lib/appUrl.js
+++ features/lib/appUrl.js
@@ -1,4 +1,5 @@
 export default (function () {
+  console.log({APP_ROOT_URL: process.env.APP_ROOT_URL})
   return process.env.APP_ROOT_URL
     ? process.env.APP_ROOT_URL
     : "http://127.0.0.1:3000";
