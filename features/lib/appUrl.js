62306d19eba3296ab8f13222786cb3f65e541e70
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-10-11 13:36:34 -0700

    Clean the file

--- features/lib/appUrl.js
+++ features/lib/appUrl.js
@@ -1,5 +1,4 @@
 export default (function () {
-  console.log({APP_ROOT_URL: process.env.APP_ROOT_URL})
   return process.env.APP_ROOT_URL
     ? process.env.APP_ROOT_URL
     : "http://127.0.0.1:3000";
