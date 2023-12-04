64e1ad43260dba69712ef972161b53975d4d1337
Zee <50284+zspencer@users.noreply.github.com>
2023-12-04 15:11:21 -0800

    🐞🔨 `Marketplace`: Don't clear `Cart` when switching `Marketplaces` (#1992)
    🐞🔨 `Marketplace`: Don't clear carts when navigating
    
    - https://github.com/zinc-collective/convene/issues/1962
    
    There was a bug we noticed during the Sutty demo where carts were being
    cleared.
    
    Turns out, if a `Marketplace` had multiple `DeliveryAreas` we would
    create a new `Cart` for the shopper every time the page loaded. Womp.
    Womp.

--- spec/furniture/marketplace/marketplace_component_spec.rb
+++ spec/furniture/marketplace/marketplace_component_spec.rb
@@ -35,4 +35,42 @@ RSpec.describe Marketplace::MarketplaceComponent, type: :component do
       end
     end
   end
+
+  describe "#cart" do
+    let(:current_person) { create(:person) }
+
+    context "when there is not a Cart for the Shopper in the Marketplace" do
+      it "creates a Cart for the Shopper" do
+        expect { component.cart }.to change { marketplace.carts.count }.by(1)
+        expect(component.cart).to be_persisted
+        expect(component.cart.delivery_area).to eql(marketplace.delivery_areas.first)
+      end
+
+      context "when the Marketplace has multiple Delivery Areas" do
+        it "creates a Cart for the Shopper without a Delivery Area" do
+          create(:marketplace_delivery_area, marketplace:)
+          expect { component.cart }.to change { marketplace.carts.count }.by(1)
+          expect(component.cart).to be_persisted
+          expect(component.cart.delivery_area).to be_nil
+        end
+      end
+    end
+
+    context "when there is a Cart for the Shopper in the Marketplace" do
+      before do
+        marketplace.carts.create(shopper: Marketplace::Shopper.create(person: current_person))
+      end
+
+      it "doesn't create another cart" do
+        expect { component.cart }.not_to change { marketplace.carts.count }
+      end
+
+      context "when the Marketplace has multiple DeliveryAreas" do
+        it "doesn't create another cart" do
+          create(:marketplace_delivery_area, marketplace:)
+          expect { component.cart }.not_to change { marketplace.carts.count }
+        end
+      end
+    end
+  end
 end
