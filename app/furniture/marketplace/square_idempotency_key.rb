9ef04c327fa64964340764b0e32eed4c52f5cccf
Ross Chapman <rosschapman@users.noreply.github.com>
2023-12-05 12:01:59 -0800

    Adds comment

--- app/furniture/marketplace/square_idempotency_key.rb
+++ app/furniture/marketplace/square_idempotency_key.rb
@@ -1,5 +1,11 @@
+# Utility for managing unique, idempotent identifiers that are associated with
+# object data we send and receive from Marketplace vendors' Square accounts.
+#
+# See: https://developer.squareup.com/docs/build-basics/common-api-patterns/idempotency
 class Marketplace
   class SquareIdempotencyKey
+    # Combines our system's Order record id with a random value under Square's
+    # max length of 192 bytes
     def self.generate(order_id)
       "#{order_id}_#{SecureRandom.hex(8)}"
     end
