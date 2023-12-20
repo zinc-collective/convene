b11f884a035d889cb41c0c3b4676648d35427d91
Zee <50284+zspencer@users.noreply.github.com>
2023-12-20 14:31:24 -0800

    🥗🌸✨ `Marketplace`: Hide `archived` `Products` from `Products#index` (#2038)
    - https://github.com/zinc-collective/convene/issues/2023
    
    Archived `Products` are no longer shown on the `Products` page.

---
+++ app/furniture/marketplace/archivable/index_link_component.rb
@@ -0,0 +1,24 @@
+class Marketplace
+  class Archivable::IndexLinkComponent < Component
+    attr_accessor :marketplace, :resource, :to_archive
+
+    def initialize(marketplace:, resource:, to_archive:, **kwargs)
+      self.marketplace = marketplace
+      self.resource = resource
+      self.to_archive = to_archive
+      super(**kwargs)
+    end
+
+    def location
+      if to_archive
+        marketplace.location(child: resource, query_params: {archive: true})
+      else
+        marketplace.location(child: resource)
+      end
+    end
+
+    def text
+      t(".#{resource}.#{to_archive ? :archive : :active}")
+    end
+  end
+end
