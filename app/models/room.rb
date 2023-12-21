a257e25a72fff3c2edb4e902c6bb3ec6ab3e7623
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-12-20 21:41:06 -0800

    Allow blank

--- app/models/room.rb
+++ app/models/room.rb
@@ -28,7 +28,7 @@ class Room < ApplicationRecord
   has_many :gizmos, dependent: :destroy, inverse_of: :room, class_name: :Furniture
   accepts_nested_attributes_for :gizmos
 
-  validates :description, length: {maximum: 300}
+  validates :description, length: {maximum: 300, allow_blank: true}
 
   def full_slug
     "#{space.slug}--#{slug}"
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Room do
   it { is_expected.to belong_to(:space).inverse_of(:rooms) }
 
   describe "#description" do
-    it { is_expected.to validate_length_of(:description).is_at_most(300) }
+    it { is_expected.to validate_length_of(:description).is_at_most(300).allow_blank }
   end
 
   describe ".slug" do
