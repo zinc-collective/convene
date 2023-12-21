228a418e59f64a267c8fc951a247ba2edd172bf2
Zee Spencer <50284+zspencer@users.noreply.github.com>
2023-12-20 22:02:19 -0800

    `SectionNavigation`: Include `Section#description` in SectionLink

--- spec/furniture/section_navigation/section_navigation_system_spec.rb
+++ spec/furniture/section_navigation/section_navigation_system_spec.rb
@@ -2,15 +2,21 @@ require "rails_helper"
 require_relative "factories"
 
 RSpec.describe SectionNavigation, type: :system do
+  let(:space) { create(:space, :with_entrance) }
+  let(:rooms) { create_list(:room, 2, space:) }
+
   it "includes a link to every section except the entrance" do
-    space = create(:space, :with_entrance)
-    rooms = create_list(:room, 2, space:)
     create(:section_navigation, room: space.entrance)
 
     visit polymorphic_path(space.entrance.location)
 
-    expect(page).to have_link(rooms.first.name, href: polymorphic_path(rooms.first.location))
-    expect(page).to have_link(rooms.last.name, href: polymorphic_path(rooms.last.location))
+    rooms.each do |room|
+      within("##{dom_id(room, :link_to)}") do
+        expect(page).to have_link(room.name, href: polymorphic_path(room.location))
+        expect(page).to have_content(rooms.description)
+      end
+    end
+
     expect(page).not_to have_link(space.entrance.name, href: polymorphic_path(space.entrance.location))
   end
 end
