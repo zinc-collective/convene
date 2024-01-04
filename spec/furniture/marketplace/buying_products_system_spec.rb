535c4fa3c628b417920851a5eed046fbf55ea969
Ross Chapman <rosschapman@users.noreply.github.com>
2024-01-03 18:36:11 -0800

    Adds debug logs

--- spec/furniture/marketplace/buying_products_system_spec.rb
+++ spec/furniture/marketplace/buying_products_system_spec.rb
@@ -57,6 +57,13 @@ describe "Marketplace: Buying Products", type: :system do
   end
 
   it "Works for Guests" do # rubocop:disable RSpec/ExampleLength
+    puts "START####################################################################################################################################################################################"
+
+    puts marketplace.square_location_id
+    puts marketplace.square_environment
+    puts marketplace.square_access_token
+
+    puts "END####################################################################################################################################################################################"
     visit(polymorphic_path(marketplace.room.location))
 
     add_product_to_cart(marketplace.products.first)
