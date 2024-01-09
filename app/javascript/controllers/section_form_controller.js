375c6c50dd11f3090a1c52955b46be9ff8d20778
Ross Chapman <rosschapman@users.noreply.github.com>
2024-01-09 13:17:37 -0800

    Hides/shows image

---
+++ app/javascript/controllers/section_form_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="section-form"
+export default class extends Controller {
+  handleMarkRemove() {
+    this.element.querySelector('img').classList.toggle('invisible')
+  }
+}
