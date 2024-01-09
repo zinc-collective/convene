f840e6eba2e77b598f3d21ab397a43a9130959bb
Ross Chapman <rosschapman@users.noreply.github.com>
2024-01-09 13:25:20 -0800

    Adds comment

--- app/javascript/controllers/section_form_controller.js
+++ app/javascript/controllers/section_form_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="section-form"
 export default class extends Controller {
+  // Toggles visibility of the Section image when selecting for removeal
   handleMarkRemove() {
     this.element.querySelector("img").classList.toggle("invisible");
   }
