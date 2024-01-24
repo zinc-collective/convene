dd584643ddc783a31c0c1e5ddbabe1f4f8142339
Ross Chapman <rosschapman@users.noreply.github.com>
2024-01-23 17:24:22 -0800

    Adds extra migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_20_034325) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_24_011436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_20_034325) do
     "expired",
     "ignored",
     "revoked",
+    "sent",
   ], force: :cascade
 
   create_enum :membership_status, [
