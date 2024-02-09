a93c848962cc8f81036e5fa05ee9b4577ed8faca
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-02-08 20:15:46 -0800

    🌸 `Section#hero_image`: takes little real-estate
    - https://github.com/zinc-collective/convene/issues/1961
    
    This brings the hero image to the Section, and makes it take up a
    reasonable amount of space.

--- app/models/media.rb
+++ app/models/media.rb
@@ -4,6 +4,7 @@ class Media < ApplicationRecord
   # We rounded up.
   # @see https://www.ios-resolution.com/
   FULL_WIDTH_16_BY_9 = [1290, 726]
+  FULL_WIDTH_SHORT = [1728, 480]
 
   # NOTE: Dependent destroy is defaulted, but when it becomes important to
   # separate the destroy request and purge operations, let's add the
