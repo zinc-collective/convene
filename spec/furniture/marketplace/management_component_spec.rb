067715ff43d32dc75c920d578769084dbbcbe253
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-02-17 14:48:04 -0800

    🌸 `Marketplace`: Improve `ManagementComponent` Iconagraphy
    - https://github.com/zinc-collective/convene/issues/2220
    
    The icons for `TaxeRates#index` and `Flyers#show` were the same, plus
    `Products#index` will conflict with `Tags#index`.
    
    So now `Products#index` is :cake: and `Flyers#show` is :qr_code:
    
    While I was doing this, I also made the following tidyings:
    
    - ✨ `SvgComponent` has `#building_storefront` icon!
    - ✨ `SvgComponent` has `#qr_code` icon!
    - 🧹 `SvgComponent` icon-method names match Heroicon's label!
    - 🧹 `SvgComponent#cake` does not need a wrapping `<svg ...>` tag!
    - 🥗 `ManagementComponent` has every link it's supposed to
    - 🧹 `ManagementComponent::Spec` uses `have_link` instead of `have_css`
    - 🧹 `ManagementComponent` eliminates usage of `ButtonComponent`
    
    We originally added the `ButtonComponent` to inject the tailwind CSS
    classes in Ruby, but with the changes @anaulin made, we now can use the
    `.button` and `.button.--primary`/`.button.--secondary` classes.
    
    Now, when I notice the `ButtonComponent` being used, I pull it out so we
    can (eventually) delete it.

--- spec/furniture/marketplace/management_component_spec.rb
+++ spec/furniture/marketplace/management_component_spec.rb
@@ -8,11 +8,12 @@ RSpec.describe Marketplace::ManagementComponent, type: :component do
   let(:component) { described_class.new(marketplace: marketplace, current_person: operator) }
   let(:marketplace) { create(:marketplace) }
 
-  it { is_expected.to have_css("a[href='#{polymorphic_path(marketplace.location(child: :products))}']", text: I18n.t("marketplace.products.index.link_to")) }
-
-  it { is_expected.to have_css("a[href='#{polymorphic_path(marketplace.location(child: :delivery_areas))}']", text: I18n.t("marketplace.delivery_areas.index.link_to")) }
-
-  it { is_expected.to have_css("a[href='#{polymorphic_path(marketplace.location(child: :tax_rates))}']", text: I18n.t("marketplace.tax_rates.index.link_to")) }
-
-  it { is_expected.to have_css("a[href='#{polymorphic_path(marketplace.location(child: :orders))}']", text: I18n.t("marketplace.orders.index.link_to")) }
+  it { is_expected.to have_link(I18n.t("marketplace.payment_settings.index.link_to"), href: polymorphic_path(marketplace.location(child: :payment_settings))) }
+  it { is_expected.to have_link(I18n.t("marketplace.products.index.link_to"), href: polymorphic_path(marketplace.location(child: :products))) }
+  it { is_expected.to have_link(I18n.t("marketplace.delivery_areas.index.link_to"), href: polymorphic_path(marketplace.location(child: :delivery_areas))) }
+  it { is_expected.to have_link(I18n.t("marketplace.tax_rates.index.link_to"), href: polymorphic_path(marketplace.location(child: :tax_rates))) }
+  it { is_expected.to have_link(I18n.t("marketplace.orders.index.link_to"), href: polymorphic_path(marketplace.location(child: :orders))) }
+  it { is_expected.to have_link(I18n.t("marketplace.notification_methods.index.link_to"), href: polymorphic_path(marketplace.location(child: :notification_methods))) }
+  it { is_expected.to have_link(I18n.t("marketplace.vendor_representatives.index.link_to"), href: polymorphic_path(marketplace.location(child: :vendor_representatives))) }
+  it { is_expected.to have_link(I18n.t("marketplace.flyers.show.link_to"), href: polymorphic_path(marketplace.location(child: :flyer))) }
 end
