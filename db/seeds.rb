dfa0603519975e3ac93efbb4b8166e9a497b4b47
Ana Ulin <ana@ulin.org>
2024-03-06 18:56:43 -0800

    Expand seeds.rb with hero images and room descriptions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,9 +25,18 @@ FactoryBot.create(
 
 SectionNavigation::SectionNavigation.create!(space:, room: space.entrance)
 
-marketplace_section = FactoryBot.create(:room, space: space, name: "Magnificent Marketplace")
+marketplace_section = FactoryBot.create(
+  :room, space: space, name: "Magnificent Marketplace",
+  description: "A marvelous marketplace for magic merchandise.",
+  hero_image: FactoryBot.create(:media)
+)
 FactoryBot.create(:marketplace, :full, room: marketplace_section)
 
-journal_section = FactoryBot.create(:room, space:, name: "Jazzy Journal")
+journal_section = FactoryBot.create(
+  :room, space:, name: "Jazzy Journal",
+  description: "Here is where I jive and jam in my journal. " \
+    "Jump in and join me as I joyfully jot down whatever I'm jazzed about just now.",
+  hero_image: FactoryBot.create(:media, :journal)
+)
 journal = FactoryBot.create(:journal, room: journal_section)
 FactoryBot.create_list(:journal_entry, 7, :with_keywords, :published, journal:)
--- spec/factories/media.rb
+++ spec/factories/media.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :media do
     upload { Rack::Test::UploadedFile.new("spec/fixtures/files/cc-kitten.jpg", "image/jpeg") }
+
+    trait :journal do
+      upload { Rack::Test::UploadedFile.new("spec/fixtures/files/cc-journal.jpg", "image/jpeg") }
+    end
   end
 end
