459242966c88495486f983fddd28b36b4e4829e2
Ana Ulin <ana@ulin.org>
2024-03-13 18:32:50 -0700

    When one of the section cards has an image, add an image placeholder to the other cards.
    When one of the section cards has a description, make sure that all cards have
    uniform heights, even if that particular card does not have a description.

---
+++ app/furniture/section_navigation/section_grid_component.rb
@@ -0,0 +1,43 @@
+class SectionNavigation
+  class SectionGridComponent < ApplicationComponent
+    delegate :policy_scope, to: :helpers
+
+    def initialize(section_navigation)
+      @section_navigation = section_navigation
+    end
+
+    def render?
+      rooms.any?
+    end
+
+    private
+
+    def description_height
+      @description_height ||= section_descriptions? ? "min-h-[120px]" : ""
+    end
+
+    def image_placeholder?(room)
+      section_images? && room.hero_image.blank?
+    end
+
+    BG_COLORS = %w[
+      bg-cyan-100 bg-emerald-100 bg-indigo-100 bg-lime-100 bg-purple-100 bg-red-100
+      bg-rose-100 bg-sky-100 bg-teal-100
+    ].freeze
+    def image_placeholder_div
+      content_tag(:div, "", class: "h-36 #{BG_COLORS.sample} m-0 p-0 rounded-t-lg w-full")
+    end
+
+    def rooms
+      @rooms = policy_scope(@section_navigation.rooms)
+    end
+
+    def section_images?
+      @section_images ||= rooms.any? { |r| r.hero_image.present? }
+    end
+
+    def section_descriptions?
+      @section_descriptions ||= rooms.any? { |r| r.description.present? }
+    end
+  end
+end
