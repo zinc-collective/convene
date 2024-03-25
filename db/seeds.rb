ea13a5c878def4809fb33f030a7184795c36945f
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-03-25 15:04:32 -0700

    🧹 `rake db:seed` is idempotent
    It turns out, running `rails db:seed` multiple times kept resulting in
    errors or duplicious data.
    
    This makes it a bit easier to run multiple times, which can be helpful
    when making a change that also wants to update the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,39 +4,47 @@
 OPERATOR_EMAIL = "operator@example.com"
 MEMBER_EMAIL = "member@example.com"
 
-FactoryBot.create(:person, :operator, name: "Ollie Operator", email: OPERATOR_EMAIL)
+Person.find_by(email: OPERATOR_EMAIL) ||
+  FactoryBot.create(:person, :operator, name: "Ollie Operator", email: OPERATOR_EMAIL)
 
-space = FactoryBot.create(:space, :with_members, :with_entrance, name: "Stevie's Space")
+space = Space.find_by(name: "Stevie's Space") ||
+  FactoryBot.create(:space, :with_members, :with_entrance, name: "Stevie's Space")
+
 space.members.first.update!(name: "Stevie Spacecat", email: MEMBER_EMAIL)
 
-FactoryBot.create(
-  :markdown_text_block,
-  room: space.entrance,
-  content: <<~MARKDOWN
-    ### Welcome to Stevie's Space!
+MarkdownTextBlock.find_by(room: space.entrance) ||
+  FactoryBot.create(
+    :markdown_text_block,
+    room: space.entrance,
+    content: <<~MARKDOWN
+      ### Welcome to Stevie's Space!
 
-    This is the entrance section. And this text is inside a markdown block.
+      This is the entrance section. And this text is inside a markdown block.
 
-    Cool, huh? 😎
+      Cool, huh? 😎
 
-    > #{Faker::Movies::HitchhikersGuideToTheGalaxy.quote}
-  MARKDOWN
-)
+      > #{Faker::Movies::HitchhikersGuideToTheGalaxy.quote}
+    MARKDOWN
+  )
 
-SectionNavigation::SectionNavigation.create!(space:, room: space.entrance)
+SectionNavigation::SectionNavigation.find_or_create_by!(room: space.entrance)
 
-marketplace_section = FactoryBot.create(
-  :room, space: space, name: "Magnificent Marketplace",
-  description: "A marvelous marketplace for magic merchandise.",
-  hero_image: FactoryBot.create(:media)
-)
+marketplace_section = space.rooms.find_by(name: "Magnificent Marketplace") ||
+  FactoryBot.create(
+    :room, space: space, name: "Magnificent Marketplace",
+    description: "A marvelous marketplace for magic merchandise.",
+    hero_image: FactoryBot.create(:media)
+  )
 FactoryBot.create(:marketplace, :full, room: marketplace_section)
 
-journal_section = FactoryBot.create(
-  :room, space:, name: "Jazzy Journal",
-  description: "Here is where I jive and jam in my journal. " \
-    "Jump in and join me as I joyfully jot down whatever I'm jazzed about just now.",
-  hero_image: FactoryBot.create(:media, :journal)
-)
-journal = FactoryBot.create(:journal, room: journal_section)
-FactoryBot.create_list(:journal_entry, 7, :with_keywords, :published, journal:)
+journal_section = space.rooms.find_by(name: "Jazzy Journal") ||
+  FactoryBot.create(
+    :room, space:, name: "Jazzy Journal",
+    description: "Here is where I jive and jam in my journal. " \
+      "Jump in and join me as I joyfully jot down whatever I'm jazzed about just now.",
+    hero_image: FactoryBot.create(:media, :journal)
+  )
+journal = Journal::Journal.find_by(room: journal_section) ||
+  FactoryBot.create(:journal, room: journal_section)
+
+FactoryBot.create_list(:journal_entry, 7, :with_keywords, :published, journal:) if journal.entries.empty?
