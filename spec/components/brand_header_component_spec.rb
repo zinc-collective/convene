a941b514d47469bd55d91da5852c33d456d2c881
Ross Chapman <rosschapman@users.noreply.github.com>
2024-04-17 14:58:47 -0700

    Adds spec

--- spec/components/brand_header_component_spec.rb
+++ spec/components/brand_header_component_spec.rb
@@ -3,13 +3,14 @@
 require "rails_helper"
 
 RSpec.describe BrandHeaderComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:space) { create(:space, :with_entrance) }
+  let!(:room) { create(:room, space: space) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders the correct room names" do
+    render_inline(described_class.new(space: space)).text
+
+    expect(page).to have_text space.name
+    expect(page).to have_text space.entrance.name
+    expect(page).to have_text room.name
+  end
 end
