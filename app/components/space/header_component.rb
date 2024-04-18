a058556b4f7caaf7f81ebecad8689e846232be98
Ross Chapman <rosschapman@users.noreply.github.com>
2024-04-18 12:37:25 -0700

    Updates conditional logic

--- app/components/space/header_component.rb
+++ app/components/space/header_component.rb
@@ -18,6 +18,6 @@ class Space::HeaderComponent < ApplicationComponent
   end
 
   def render?
-    @space.present? && @space.entrance.present?
+    @space.present? && @space.entrance_id.present?
   end
 end
