e2dbfbb508da7fd1d854c3a3d5027574f1664ba6
Ross Chapman <rosschapman@users.noreply.github.com>
2024-05-22 14:30:22 -0700

    Fixes spec

---
+++ spec/factories/marketplace/tags.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :marketplace_tag, class: "Marketplace::Tag" do
+    marketplace
+  end
+end
--- spec/furniture/marketplace/tag_spec.rb
+++ spec/furniture/marketplace/tag_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Marketplace::Tag, type: :model do
+  subject { build(:marketplace_tag) }
+
   it { is_expected.to validate_uniqueness_of(:label).case_insensitive.scoped_to(:marketplace_id) }
 end
