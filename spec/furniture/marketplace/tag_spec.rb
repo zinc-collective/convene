d6559174124e1315837bb0ad3fc3c4ae03e17d8b
Ross Chapman <rosschapman@users.noreply.github.com>
2024-05-22 13:59:18 -0700

    Updates model spec

--- spec/furniture/marketplace/tag_spec.rb
+++ spec/furniture/marketplace/tag_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
 RSpec.describe Marketplace::Tag, type: :model do
-  it { is_expected.to validate_uniqueness_of(:label).case_insensitive.scoped_to(:bazaar_id) }
+  it { is_expected.to validate_uniqueness_of(:label).case_insensitive.scoped_to(:marketplace_id) }
 end
