94ea17df3a355094568fe1fa2b0391f4881cbac2
Ross Chapman <rosschapman@users.noreply.github.com>
2024-05-22 18:31:55 -0700

    Updates styling and adds test placeholder

--- app/furniture/marketplace/tag.rb
+++ app/furniture/marketplace/tag.rb
@@ -10,7 +10,7 @@ class Marketplace
 
     location(parent: :marketplace)
 
-    positioned on: :bazaar
+    positioned on: :marketplace
 
     # Tacking `_tag` onto the end of this scope name solely to avoid
     # collisions with ActiveRecord `groups`
