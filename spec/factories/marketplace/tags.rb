6e58f3c5056dacdddb83cb11150d06db337b41ff
Ana Ulin <ana@ulin.org>
2024-05-23 19:04:05 -0700

    Revert "Space admins can create "Menu/Product Groups" and reorder them for display on their marketplace" (#2422)
    Revert "Space admins can create "Menu/Product Groups" and reorder them for di…"
    
    This reverts commit 67dc740061403e4c813c659e8c906f9523af31a0.

--- spec/factories/marketplace/tags.rb
+++ spec/factories/marketplace/tags.rb
@@ -1,11 +1,6 @@
 FactoryBot.define do
   factory :marketplace_tag, class: "Marketplace::Tag" do
-    marketplace { association(:marketplace) }
     sequence(:label) { |n| "#{Faker::Food.allergen} #{n}" }
-    sequence(:position) { |n| n }
-
-    trait :group do
-      is_group { true }
-    end
+    marketplace
   end
 end
