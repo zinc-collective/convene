70b03826ac6852433222caf822c1b34109afc8d3
Ana Ulin <ana@ulin.org>
2024-05-23 19:14:30 -0700

    Fix migration order, and contents of migration to refer to the correct column for the tag position scope.

--- db/migrate/20240524021224_add_position_index_to_marketplace_tags.rb
+++ db/migrate/20240524021224_add_position_index_to_marketplace_tags.rb
@@ -2,6 +2,6 @@ class AddPositionIndexToMarketplaceTags < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    add_index :marketplace_tags, [:bazaar_id, :position], unique: true, algorithm: :concurrently
+    add_index :marketplace_tags, [:marketplace_id, :position], unique: true, algorithm: :concurrently
   end
 end
