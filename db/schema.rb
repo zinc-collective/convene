3af48e92a147a4ce6f5d2399eaeff0d2a372d141
Ross Chapman <rosschapman@users.noreply.github.com>
2024-05-29 18:08:44 -0700

    Adds position index to furnitures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_30_003329) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_010707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_003329) do
     t.datetime "updated_at", null: false
     t.uuid "room_id"
     t.text "secrets_ciphertext"
+    t.index ["room_id", "position"], name: "index_furnitures_on_room_id_and_position", unique: true
     t.index ["room_id"], name: "index_furnitures_on_room_id"
   end
 
