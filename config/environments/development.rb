b31a03b4e74723d9c937dce4481e438d9a2bf1f6
Ross Chapman <rosschapman@users.noreply.github.com>
2024-06-05 17:39:30 -0700

    Fixes formatting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,12 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
+    Bullet.enable = true
+    Bullet.alert = true
     Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
