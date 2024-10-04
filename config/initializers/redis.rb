a68f98e643dec933447cac3d4ca2d3dfec5815a5
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-10-03 17:41:06 -0700

    Only do this in prod, since Test doesn't use Redis and local doesn't care

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-$redis = # rubocop:disable Style/GlobalVars
-  Redis.new(url: ENV["REDIS_URL"],
-    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE})
+if Rails.env.production?
+  $redis = # rubocop:disable Style/GlobalVars
+    Redis.new(url: ENV["REDIS_URL"],
+      ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE})
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,15 @@
-Sidekiq.configure_server do |config|
-  config.redis = {
-    url: ENV["REDIS_URL"],
-    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
-  }
-end
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+    }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = {
-    url: ENV["REDIS_URL"],
-    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
-  }
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+    }
+  end
 end
