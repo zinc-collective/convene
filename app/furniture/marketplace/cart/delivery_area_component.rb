06a8bb63ee8e770b9f0103b8b2cd19fa5900dc56
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-10-09 18:52:35 -0700

    Show the delivery fee to the user in the delivery area picker

--- app/furniture/marketplace/cart/delivery_area_component.rb
+++ app/furniture/marketplace/cart/delivery_area_component.rb
@@ -12,6 +12,24 @@ class Marketplace
       super(cart, :delivery_area)
     end
 
+    def price
+      helpers.humanized_money_with_symbol(delivery_area.price)
+    end
+
+    def fee_as_percentage
+      "#{helpers.number_to_percentage(delivery_area.fee_as_percentage, precision: 0)} of subtotal"
+    end
+
+    def fee_description
+      if delivery_area.charges_fee_as_percentage? && delivery_area.charges_fee_as_price?
+        "#{price} plus #{fee_as_percentage}"
+      elsif !delivery_area.charges_fee_as_percentage? && delivery_area.charges_fee_as_price?
+        price
+      elsif delivery_area.charges_fee_as_percentage? && !delivery_area.charges_fee_as_price?
+        fee_as_percentage
+      end
+    end
+
     def single_delivery_area?
       cart.marketplace.delivery_areas.unarchived.size == 1
     end
