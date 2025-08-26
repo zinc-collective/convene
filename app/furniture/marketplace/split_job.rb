e787d254e8328bd952396e0a5ce98626dc1a8085
Zee Spencer <50284+zspencer@users.noreply.github.com>
2025-08-25 18:21:37 -0700

    Marketplace: Handle insufficient balances gracefully
    Fixes https://github.com/zinc-collective/convene/issues/3057
    
    Payments take time to clear, so if a big order comes in or the balance
    is low we can't actually split immediately.
    
    It may also make sense for us to wait to do the split until end-of-day
    in general, rather than immediately upon receiving the payment...
    
    In cases where the pending and available balance are high enough, we
    re-enqueue and don't cause an error (or tick the "5 tries until the job
    dies") counter.
    
    If the pending and available balance together won't cover it, we raise
    an error and tick that counter up, which may result in un-split orders;
    but does mean we don't keep trying to split an order that will never be
    funded.

--- app/furniture/marketplace/split_job.rb
+++ app/furniture/marketplace/split_job.rb
@@ -15,6 +15,27 @@ class Marketplace
         return
       end
 
+      balance = Stripe::Balance.retrieve(api_key: stripe_api_key)
+      total_available_balance =
+        balance.available.sum { |balance| balance.amount }
+      total_pending_balance =
+        balance.pending.sum { |balance| balance.amount }
+
+      if total_available_balance >= vendors_share.cents
+        initiate_transfer
+      else
+        order.events.create(description: "Available balance of #{total_available_balance} insufficient to cover vendors share of #{order.vendors_share.cents}")
+        if total_available_balance + total_pending_balance >= vendors_share.cents
+          reschedule_split
+        else
+          order.events.create(description:
+        "Pending balance (#{total_pending_balance}) and Available balance (#{total_available_balance}) does not covers vendors share #{order.vendors_share.cents}. Add funds to your Stripe Account so your Vendors get paid!")
+          raise InsufficientBalanceError, "Available Balance: #{total_available_balance}, Pending Balance: #{total_pending_balance}, Vendors Share: #{order.vendors_share.cents}"
+        end
+      end
+    end
+
+    private def initiate_transfer
       transfer = Stripe::Transfer.create({
         amount: vendors_share.cents,
         currency: "usd",
@@ -25,5 +46,13 @@ class Marketplace
       order.update(stripe_transfer_id: transfer.id)
       order.events.create(description: "Payment Split Completed")
     end
+
+    private def reschedule_split
+      order.events.create(description:
+        "Summing Pending and Available balances covers vendors share. Deferring split until #{24.hours.from_now.end_of_day}")
+      SplitJob.set(wait_until: 24.hours.from_now.end_of_day).perform_later(order: order)
+    end
+
+    class InsufficientBalanceError < StandardError; end
   end
 end
